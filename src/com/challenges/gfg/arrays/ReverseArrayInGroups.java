454779b0ec5804d5151c907eec52894d6fe5cde8
govindev <vgovindarajuyadav@gmail.com>
2020-02-12 07:06:46 +0530

    Reverse array in groups
    Signed-off-by: govindev <vgovindarajuyadav@gmail.com>
    https://practice.geeksforgeeks.org/problems/reverse-array-in-groups/0

---
+++ src/com/challenges/gfg/arrays/ReverseArrayInGroups.java
@@ -0,0 +1,54 @@
+package com.challenges.gfg.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Given an array arr[] of positive integers of size N. Reverse every sub-array of K group elements.
+//
+//Input:
+//The first line of input contains a single integer T denoting the number of test cases. Then T test cases follow. Each test case consist of two lines of input. The first line of each test case consists of an integer N(size of array) and an integer K separated by a space. The second line of each test case contains N space separated integers denoting the array elements.
+//
+//Output:
+//For each test case, print the modified array.
+//
+//Constraints:
+//1 ≤ T ≤ 200
+//1 ≤ N, K ≤ 107
+//1 ≤ A[i] ≤ 1018
+//
+//Example:
+//Input
+//2
+//5 3
+//1 2 3 4 5
+//6 2
+//10 20 30 40 50 60
+//
+//Output
+//3 2 1 5 4
+//20 10 40 30 60 50
+//
+//Explanation:
+//Testcase 1: Reversing groups in size 3, first group consists of elements 1, 2, 3. Reversing this group, we have elements in order as 3, 2, 1.
+
+public class ReverseArrayInGroups {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			int subArrInd = Integer.parseInt(br.readLine().split(" ")[1]) - 1;
+			String[] array = br.readLine().split(" ");
+			for (int j = 0, k = subArrInd; j < array.length; j++, k = k - 2) {
+				k = ((j + k) > (array.length - 1)) ? ((array.length - 1) - j) : k;
+				sb.append(array[j + k] + " ");
+				k = (k == (-subArrInd)) ? subArrInd + 2 : k;
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
