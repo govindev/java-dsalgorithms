445dd6cd87f2e3412b500ba5179a8c4bbde5f09a
govindev <vgovindarajuyadav@gmail.com>
2020-02-13 11:56:32 +0530

    Pythagorean Triplet
    Signed-off-by: govindev <vgovindarajuyadav@gmail.com>
    https://practice.geeksforgeeks.org/problems/pythagorean-triplet/0

---
+++ src/com/challenges/gfg/arrays/PythagoreanTriplet.java
@@ -0,0 +1,65 @@
+package com.challenges.gfg.arrays;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+// https://practice.geeksforgeeks.org/problems/pythagorean-triplet/0
+//Given an array of integers, write a function that returns true if there is a triplet (a, b, c) that satisfies a2 + b2 = c2.
+//
+//Input:
+//The first line contains T, denoting the number of testcases. Then follows description of testcases. Each case begins with a single positive integer N denoting the size of array. The second line contains the N space separated positive integers denoting the elements of array A.
+//
+//Output:
+//For each testcase, print "Yes" or  "No" whether it is Pythagorean Triplet or not (without quotes).
+//
+//Constraints:
+//1 <= T <= 100
+//1 <= N <= 107
+//1 <= A[i] <= 1000
+//
+//Example:
+//Input:
+//1
+//5
+//3 2 4 6 5
+//
+//Output:
+//Yes
+//
+//Explanation:
+//Testcase 1: a=3, b=4, and c=5 forms a pythagorean triplet, so we print "Yes"
+
+public class PythagoreanTriplet {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			boolean sumFound = false;
+			int length = Integer.parseInt(br.readLine());
+			String[] a = br.readLine().split(" ");
+			for (int k = 0; k < length - 1; k++) {
+				for (int l = 1; l < length; l++) {
+					int kVal = Integer.parseInt(a[k]) * Integer.parseInt(a[k]);
+					int lVal = Integer.parseInt(a[l]) * Integer.parseInt(a[l]);
+					int sum = kVal + lVal;
+					for (int m = 0; m < length; m++) {
+						int mVal = Integer.parseInt(a[m]) * Integer.parseInt(a[m]);
+						if (mVal == sum) {
+							sumFound = true;
+							break;
+						}
+					}
+					if (sumFound)
+						break;
+				}
+				if (sumFound)
+					break;
+			}
+			sb.append((sumFound) ? "Yes\n" : "No\n");
+		}
+		System.out.println(sb);
+	}
+}
--- src/com/challenges/gfg/arrays/TrappingRainWater.java
+++ src/com/challenges/gfg/arrays/TrappingRainWater.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+// https://practice.geeksforgeeks.org/problems/trapping-rain-water/0
+	
 //Given an array arr[] of N non-negative integers representing height of blocks at index i as Ai where the width of each block is 1. Compute how much water can be trapped in between blocks after raining.
 //Structure is like below:
 //|  |
