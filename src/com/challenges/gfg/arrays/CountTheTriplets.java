1a2bae3193110c15b41f23d4e7d7a35e3d43d8d6
govindev <vgovindarajuyadav@gmail.com>
2020-02-19 10:48:17 +0530

    Count the triplets
    Signed-off-by: govindev <vgovindarajuyadav@gmail.com>
    https://practice.geeksforgeeks.org/problems/count-the-triplets/0

--- src/com/challenges/gfg/arrays/CountTheTriplets.java
+++ src/com/challenges/gfg/arrays/CountTheTriplets.java
@@ -1,77 +1,63 @@
 package com.challenges.gfg.arrays;
 
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-//Given an array of distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
-//
-//Input:
-//The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. First line of each test case contains an Integer N denoting size of array and the second line contains N space separated elements.
-//
-//Output:
-//For each test case, print the count of all triplets, in new line. If no such triplets can form, print "-1".
-//
-//Constraints:
-//1 <= T <= 100
-//3 <= N <= 105
-//1 <= A[i] <= 106
-//
-//Example:
-//Input:
-//2
-//4
-//1 5 3 2
-//3
-//3 2 7
-//Output:
-//2
-//-1
-//
-//Explanation:
-//Testcase 1: There are 2 triplets: 1 + 2 = 3 and 3 +2 = 5
-
+/**
+ * https://practice.geeksforgeeks.org/problems/count-the-triplets/0
+ * 
+ * Given an array of distinct integers. The task is to count all the triplets
+ * such that sum of two elements equals the third element.
+ * 
+ * Input: The first line of input contains an integer T denoting the number of
+ * test cases. Then T test cases follow. Each test case consists of two lines.
+ * First line of each test case contains an Integer N denoting size of array and
+ * the second line contains N space separated elements.
+ * 
+ * Output: For each test case, print the count of all triplets, in new line. If
+ * no such triplets can form, print "-1".
+ * 
+ * Constraints: 1 <= T <= 100 3 <= N <= 105 1 <= A[i] <= 106
+ * 
+ * Example: Input: 2 4 1 5 3 2 3 3 2 7 Output: 2 -1
+ * 
+ * Explanation: Testcase 1: There are 2 triplets: 1 + 2 = 3 and 3 +2 = 5
+ * 
+ * @author Govind
+ *
+ */
 public class CountTheTriplets {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		if (!(1 <= t && t <= 100)) {
-			System.out.println("Value must be between 1 and 100. Exiting");
-			return;
-		}
-		StringBuffer sb = new StringBuffer("");
-		for (int z = 0; z < t; z++) {
-			int n = sc.nextInt();
-			if (!(3 <= n && n <= 100000)) {
-				System.out.println("Value must be between 3 and 105. Exiting");
-				return;
-			}
-			int[] a = new int[n];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder output = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		for (int p = 0; p < t; p++) {
+			int n = Integer.parseInt(br.readLine());
+			String[] aStr = br.readLine().trim().split(" ");
+			int[] a = new int[aStr.length];
 			for (int i = 0; i < a.length; i++) {
-				a[i] = sc.nextInt();
-				if (!(1 <= a[i] && a[i] <= 1000000)) {
-					System.out.println("Value must be between 1 and 106. Exiting");
-					return;
-				}
+				a[i] = Integer.parseInt(aStr[i]);
 			}
-			int count = 0;
-			for (int i = 0; i < a.length - 1; i++) {
-				for (int j = i + 1; j < a.length; j++) {
-					int sum = a[i] + a[j];
-					for (int k = 0; k < a.length; k++) {
+			Arrays.sort(a);
+			int tripletsCount = 0;
+			for (int i = 0; i < n - 1; i++) {
+				int iVal = a[i];
+				for (int j = i + 1; j < n; j++) {
+					int sum = iVal + a[j];
+					for (int k = j + 1; k < n; k++) {
 						if (sum == a[k]) {
-							// Found one
-							count++;
+							tripletsCount++;
 						}
+						if (sum < a[k])
+							break;
 					}
 				}
 			}
-			if (count == 0) {
-				count = -1;
-			}
-			sb.append(count + "\n");
+			tripletsCount = (tripletsCount <= 0) ? -1 : tripletsCount;
+			output.append(tripletsCount + "\n");
 		}
-		System.out.println(sb);
-		sc.close();
+		System.out.println(output);
 	}
-
 }
