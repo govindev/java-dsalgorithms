9528e9ba0c335657a6c9175d562e8ff0846a58fa
govindev <vgovindarajuyadav@gmail.com>
2020-02-21 12:43:29 +0530

    Rotate a Linked List
    Signed-off-by: govindev <vgovindarajuyadav@gmail.com>
    https://practice.geeksforgeeks.org/problems/rotate-a-linked-list/1

--- src/com/challenges/gfg/linkedlist/LinkedList.java
+++ src/com/challenges/gfg/linkedlist/LinkedList.java
@@ -13,4 +13,31 @@ public class LinkedList {
 		}
 	}
 
+	public StringBuilder getPrintString() {
+		StringBuilder sb = new StringBuilder();
+		Node current = head;
+		while (current.next != null) {
+			sb.append(current.data + " ");
+			current = current.next;
+		}
+		sb.append(current.data);
+		return sb;
+	}
+
+	public void roatate(int noOfRoatations) {
+		Node current = head;
+		while (current.next != null) {
+			current = current.next;
+		}
+		// 'Last' is the 'current' element where 'current.next' is null
+		// So now 'last' is nothing but 'current'
+		Node last = current;
+		for (int i = 0; i < noOfRoatations; i++) {
+			last.next = head;
+			head = head.next;
+			last = last.next;
+			last.next = null;
+		}
+	}
+
 }
