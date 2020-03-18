b76cdd321584ebb598dc9f70d4f2d34182876c38
govindev <vgovindarajuyadav@gmail.com>
2020-03-18 07:01:58 +0530

    Heap Sort
    Change-Id: I011c5138e7b74fd81231da996c5a04bd6c6697e1
    Signed-off-by: govindev <vgovindarajuyadav@gmail.com>

--- src/com/challenges/gfg/heap/HeapSort.java
+++ src/com/challenges/gfg/heap/HeapSort.java
@@ -2,6 +2,31 @@ package com.challenges.gfg.heap;
 
 import java.util.*;
 
+/**
+ * https://practice.geeksforgeeks.org/problems/heap-sort/1
+ * 
+ * Given an array of size N. The task is to sort the array elements by
+ * completing functions heapify() and buildHeap() which are used to implement
+ * Heap Sort.
+ * 
+ * Input: First line of the input denotes number of test cases T. First line of
+ * the test case is the size of array and second line consists of array
+ * elements.
+ * 
+ * Output: Sorted array in ascending order.
+ * 
+ * Constraints: 1 <= T <= 100 1 <= N <= 106 1 <= arr[i] <= 106
+ * 
+ * Example: Input: 2 5 4 1 3 9 7 10 10 9 8 7 6 5 4 3 2 1
+ * 
+ * Output: 1 3 4 7 9 1 2 3 4 5 6 7 8 9 10
+ * 
+ * Explanation: Testcase 1: After sorting elements using heap sort, elements
+ * will be in order as 1, 3, 4, 7 and 9.
+ * 
+ * @author Govind
+ *
+ */
 class HeapSort {
 	void printArray(int arr[], int n) {
 		// int n = arr.length;
@@ -44,7 +69,7 @@ class GfG {
 			int current = i;
 			while (true) {
 				// Fix the heap
-				// Find the root
+				// Find the parent
 				int parent = 0;
 				if (current % 2 == 0) {
 					parent = (current / 2) - 1;
@@ -58,42 +83,49 @@ class GfG {
 					arr[parent] = arr[current];
 					arr[current] = temp;
 
-					if (parent == 0)
+					// NOTE : Having (parent == 0) didn't work. Time complexity error is coming
+					if (parent <= 0)
 						break;
 					current = parent;
 				}
 			}
-
 		}
 	}
 
 	// To heapify a subtree rooted with node i which is
-	// an index in arr[]. n is size of heap 1 9 3 7 7 
+	// an index in arr[]. n is size of heap 1 9 3 7 7
 	void heapify(int arr[], int n, int i) {
-		int currentIndex = 0;
-		while (true) {
-			int current = arr[currentIndex];
-			int leftIndex = (2 * currentIndex) + 1;
-			int rightIndex = (2 * currentIndex) + 2;
-			if (leftIndex >= n || rightIndex >= n || leftIndex == i)
-				break;
-			int left = arr[leftIndex];
-			int right = arr[rightIndex];
-			if (rightIndex == i && arr[currentIndex] > left)
-				break;
-			if (arr[currentIndex] > left && arr[currentIndex] > right)
-				break;
-			if (rightIndex == i || left > right) {
+		if (n == 0 || i >= n)
+			return;
+		int current = arr[i];
+		int leftIndex = (2 * i) + 1;
+		int rightIndex = (2 * i) + 2;
+		if (leftIndex >= n)
+			return;
+		int left = arr[leftIndex];
+		int right = arr[rightIndex];
+		if (rightIndex == n) {
+			if (current > left)
+				return;
+			else {
 				// Exchange current with left
-				arr[currentIndex] = left;
+				arr[i] = left;
 				arr[leftIndex] = current;
-				currentIndex = leftIndex;
-			} else {
-				// Exchange current with right
-				arr[currentIndex] = right;
-				arr[rightIndex] = current;
-				currentIndex = rightIndex;
+				return;
 			}
+		}
+		if (current > left && current > right)
+			return;
+		if (left > right) {
+			// Exchange current with left
+			arr[i] = left;
+			arr[leftIndex] = current;
+			heapify(arr, n, leftIndex);
+		} else {
+			// Exchange current with right
+			arr[i] = right;
+			arr[rightIndex] = current;
+			heapify(arr, n, rightIndex);
 		}
 	}
 }
