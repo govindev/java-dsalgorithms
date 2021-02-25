cfa63c5e81d8eed5a0325fb2904fd97eefe74065
govindev <vgovindarajuyadav@gmail.com>
2021-02-25 22:08:30 +0530

    Shortest Unsorted Continuous Sub array
    Change-Id: I81928b0b647f089d0f7a40c938ee8364fdb0e2d8

---
+++ src/com/challenges/leetcode/twentyone/feb/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,44 @@
+package com.challenges.leetcode.twentyone.feb;
+
+/***
+ * https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3652/
+ * 
+ * Shortest unsorted continuous sub array
+ * 
+ * @author Govind
+ *
+ */
+public class ShortestUnsortedContinuousSubarray {
+	public int findUnsortedSubarray(int[] nums) {
+		int sortingStart = -1;
+		int sortingEnd = -1;
+		int greatest = nums[0];
+		for (int i = 0; i < nums.length; i++) {
+			Boolean isSorted = true;
+			if (i > 0 && ((nums[i] < nums[i - 1]) || (nums[i] < greatest))) {
+				isSorted = false;
+				sortingStart = (sortingStart > -1) ? sortingStart : i - 1;
+				if (sortingStart == i - 1)
+					for (int j = i - 2; j >= 0; j--) {
+						if (nums[i - 1] == nums[j]) {
+							sortingStart = j;
+						} else
+							break;
+					}
+				sortingEnd = i;
+			}
+			if (nums[i] > greatest) {
+				greatest = nums[i];
+			}
+			if (i < (nums.length - 1) && nums[i] > nums[i + 1]) {
+				isSorted = false;
+				sortingEnd = i + 1;
+				sortingStart = (sortingStart > -1) ? sortingStart : i;
+			}
+		}
+		if (sortingEnd >= 0 && sortingStart >= 0) {
+			return sortingEnd - sortingStart + 1;
+		}
+		return 0;
+	}
+}
