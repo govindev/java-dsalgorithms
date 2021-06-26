f05b2509c8b680ee1e0854a355c717b46c379784
govindev <vgovindarajuyadav@gmail.com>
2021-06-26 10:32:15 +0530

    Add Two Numbers - Effective solution

--- src/com/leetcode/topinterviewquestions/AddTwoNumbers.java
+++ src/com/leetcode/topinterviewquestions/AddTwoNumbers.java
@@ -42,9 +42,40 @@ public class AddTwoNumbers {
 		System.out.println(output.toString());
 	}
 
-	public ListNode addTwoNumbersEffective() {
+	public ListNode addTwoNumbersEffective(ListNode l1, ListNode l2) {
 		// Effective Solution
-		return null;
+		int carry = 0;
+        ListNode root = null;
+        ListNode current = null;
+        while(l1 != null || l2 != null) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            ListNode node = new ListNode(carry % 10);
+            if (root == null) {
+                root = node;
+                current = node;
+            } else {
+                current.next = node;
+                current = node;
+            }
+            carry = carry / 10;
+        }
+        if (carry != 0) {
+            ListNode node = new ListNode(carry);
+            if (root == null) {
+                root = node;
+            } else {
+                current.next = node;
+                current = node;
+            }
+        }
+        return root;
 	}
 
 	private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
