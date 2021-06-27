88d42dbe0f4cd297ea695d4251f88719e238a5a3
govindev <vgovindarajuyadav@gmail.com>
2021-06-27 13:14:22 +0530

    Better solution

--- src/com/leetcode/topinterviewquestions/LongestPalindromeSubstring.java
+++ src/com/leetcode/topinterviewquestions/LongestPalindromeSubstring.java
@@ -2,20 +2,36 @@ package com.leetcode.topinterviewquestions;
 
 public class LongestPalindromeSubstring {
 	public static void main(String[] args) {
-		String s = "cbbd";
+		String s = "babab";
 		// System.out.println(isPalindrome(s, 0, s.length() - 1));
-		System.out.println(longestPalindrome(s));
+		System.out.println(longestPalindromeBetter1(s));
 	}
 
-	private static String longestPalindrome(String s) {
+	private static String longestPalindrome(String s, int left, int right) {
+		if (left == right) {
+			return s.charAt(left) + "";
+		}
+		if (left > right) {
+			return null;
+		}
+		if (isPalindrome(s, left, right)) {
+			return s.substring(left, right + 1);
+		} else {
+			String longestPalindrome1 = longestPalindrome(s, left, right - 1);
+			String longestPalindrome2 = longestPalindrome(s, left + 1, right);
+			return longestPalindrome1.length() > longestPalindrome2.length() ? longestPalindrome1 : longestPalindrome2;
+		}
+	}
+
+	private static String longestPalindromeBetter1(String s) {
+		return longestPalindrome(s, 0, s.length() - 1);
+	}
+
+	public String longestPalindromeOld(String s) {
 		int longestPalStart = 0, longestPalEnd = 0;
 
 		for (int i = 0; i < s.length() - 1; i++) {
 			for (int j = s.length() - 1; j >= i + 1; j--) {
-				System.out.println("i : " + i + " j : " + j);
-				
-				
-				
 				if (isPalindrome(s, i, j)) {
 					System.out.println("Palindrome : " + s.substring(i, j + 1));
 					if (j - i + 1 > longestPalEnd - longestPalStart + 1) {
