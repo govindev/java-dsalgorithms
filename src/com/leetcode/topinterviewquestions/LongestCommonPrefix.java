96d6cf52c1c741dc2978ce31ed82883e4319a3d0
govindev <vgovindarajuyadav@gmail.com>
2021-07-02 20:38:33 +0530

    Longest common prefix clean code

--- src/com/leetcode/topinterviewquestions/LongestCommonPrefix.java
+++ src/com/leetcode/topinterviewquestions/LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package com.leetcode.topinterviewquestions;
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class LongestCommonPrefix {
 		System.out.println(longestCommonPrefix(strs));
 	}
 
-	public static String longestCommonPrefix(String[] strs) {
+	public static String longestCommonPrefixNaive(String[] strs) {
 		int ssi = 0;
 		if (strs.length == 1)
 			return strs[0];
@@ -21,5 +23,24 @@ public class LongestCommonPrefix {
 			}
 			ssi++;
 		}
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+		// Clean Code
+		if (strs.length == 1)
+			return strs[0];
+		Arrays.sort(strs);
+		int i = 0;
+		String firstStr = strs[0];
+		String lastStr = strs[strs.length - 1];
+		while (i < firstStr.length()) {
+			if (firstStr.charAt(i) == lastStr.charAt(i)) {
+				i++;
+			} else {
+				break;
+			}
+		}
+		return firstStr.substring(0, i);
+
 	}
 }
