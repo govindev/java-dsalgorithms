035b229817279548e106beeedb906dafd1c1e8a9
govindev <vgovindarajuyadav@gmail.com>
2021-07-04 01:06:58 +0530

    Implement str str : Knuth-Morris-Pratt(KMP) algorithm approach

--- src/com/leetcode/topinterviewquestions/ImplementStrStr.java
+++ src/com/leetcode/topinterviewquestions/ImplementStrStr.java
@@ -2,12 +2,64 @@ package com.leetcode.topinterviewquestions;
 
 public class ImplementStrStr {
 	public static void main(String[] args) {
-		String haystack = "eeeeeb";
-		String needle = "eeeb";
-		System.out.println(strStrNaive(haystack, needle));
+		String haystack = "adsgwadsxdsgwadsgz";
+		String needle = "dsgwadsgz";
+		System.out.println(strStr(haystack, needle));
 	}
 
+	public static int[] constructLookupArray(String pattern) {
+		// Construct and return the lookup array
+		int[] lookup = new int[pattern.length()];
+		if (pattern.length() < 1)
+			return lookup;
+		int i = 0;
+		lookup[i] = 0;
+		for (int j = 1; j < pattern.length();) {
+			if (pattern.charAt(i) == pattern.charAt(j)) {
+				lookup[j] = i + 1;
+				i++;
+				j++;
+			} else {
+				if (i != 0) {
+					i = lookup[i - 1];
+				} else {
+					lookup[j] = 0;
+					j++;
+				}
+			}
+		}
+
+		return lookup;
+	}
+
+	public static int strStr(String haystack, String needle) {
+		if (haystack.equals(needle) || needle.equals(""))
+			return 0;
+		int[] lookup = constructLookupArray(needle);
+		int j = 0;
+		int hl = haystack.length();
+		int nl = needle.length();
+		for (int i = 0; i < hl && j < nl; i++) {
+			if (haystack.charAt(i) == needle.charAt(j)) {
+				j++;
+				if (j == needle.length()) {
+					return i - j + 1;
+				}
+			} else {
+				if (j > 0) {
+					j = lookup[j - 1];
+					i--;
+				}
+
+			}
+		}
+		return -1;
+	}
+
+	// IGNORE BELOW.
+
 	public static int strStrNaive(String haystack, String needle) {
+		// Naive solution. time complexity is order of m * n
 		int hl = haystack.length();
 		int nl = needle.length();
 		if (needle.equals(haystack))
