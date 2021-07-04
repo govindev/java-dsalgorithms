9ad7e48e6cdcdfca59277dc16153ff89d575d807
govindev <vgovindarajuyadav@gmail.com>
2021-07-04 12:10:48 +0530

    Divide two integers effective way

--- src/com/leetcode/topinterviewquestions/DivideTwoIntegers.java
+++ src/com/leetcode/topinterviewquestions/DivideTwoIntegers.java
@@ -9,6 +9,27 @@ public class DivideTwoIntegers {
 	}
 
 	private static int divide(int dividend, int divisor) {
+		// Double the count
+		if (dividend == 1 << 31 && divisor == -1)
+			return Integer.MAX_VALUE;
+
+		boolean sign = (dividend >= 0) == (divisor >= 0) ? true : false;
+		dividend = Math.abs(dividend);
+		divisor = Math.abs(divisor);
+
+		int result = 0;
+		while (dividend - divisor >= 0) {
+			int count = 0;
+			while (dividend - (divisor << 1 << count) >= 0) {
+				count++;
+			}
+			result += 1 << count;
+			dividend -= divisor << count;
+		}
+		return sign ? result : -result;
+	}
+
+	private static int divideNaive(int dividend, int divisor) {
 		// Brute force
 		// Wrong way as we are using multiplication, but we are not supposed to use it
 		long m = (divisor > 0) ? 1 : -1;
