9ff47fedbdc6824419cd4288892cf1ecff5ae497
govindev <vgovindarajuyadav@gmail.com>
2021-07-06 10:47:29 +0530

    First Missing Postivie -Best solution

--- src/com/leetcode/topinterviewquestions/FirstMissingPositive.java
+++ src/com/leetcode/topinterviewquestions/FirstMissingPositive.java
@@ -4,10 +4,36 @@ import java.util.Arrays;
 
 public class FirstMissingPositive {
 	public static void main(String[] args) {
-
+		System.out.println(firstMissingPositive(new int[] { 3, 4, -1, 1 }));
 	}
 
-	public int firstMissingPositive(int[] nums) {
+	public static int firstMissingPositive(int[] nums) {
+		int i = 0;
+		while (i < nums.length) {
+			if (nums[i] < 1 || nums[i] > nums.length || nums[i] == i + 1) {
+				i++;
+				continue;
+			}
+
+			int e = nums[i];
+			if (nums[e - 1] != e) {
+				// Swap
+				nums[i] = nums[e - 1];
+				nums[e - 1] = e;
+			} else
+				i++;
+		}
+		i = 0;
+		for (; i < nums.length; i++)
+
+		{
+			if (nums[i] != i + 1)
+				return i + 1;
+		}
+		return i + 1;
+	}
+
+	public int firstMissingPositiveBetter(int[] nums) {
 		Arrays.sort(nums);
 		int i = 0;
 		while (i < nums.length && nums[i] < 1) {
