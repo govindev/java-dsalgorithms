ca8f1f247ef87c9f3c26bfe92e23319af8d7a4d7
govindev <vgovindarajuyadav@gmail.com>
2021-07-10 00:06:37 +0530

    Code optimization

--- src/com/leetcode/topinterviewquestions/PlusOne.java
+++ src/com/leetcode/topinterviewquestions/PlusOne.java
@@ -7,6 +7,21 @@ public class PlusOne {
 	}
 
 	public static int[] plusOne(int[] digits) {
+		// Most optimized
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] + 1 > 9) {
+				digits[i] = 0;
+			} else {
+				digits[i] += 1;
+				return digits;
+			}
+		}
+		int[] output = new int[digits.length + 1];
+		output[0] = 1;
+		return output;
+	}
+
+	public static int[] plusOne1(int[] digits) {
 		int overflow = 1;
 		for (int i = digits.length - 1; i >= 0; i--) {
 			int v = digits[i] + overflow;
