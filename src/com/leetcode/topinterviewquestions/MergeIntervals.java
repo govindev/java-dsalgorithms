8b04552b66be777b9287196f2218cd955fd44e31
govindev <vgovindarajuyadav@gmail.com>
2021-07-11 13:07:46 +0530

    Merge Intervals - Much cleaner way

--- src/com/leetcode/topinterviewquestions/MergeIntervals.java
+++ src/com/leetcode/topinterviewquestions/MergeIntervals.java
@@ -8,20 +8,41 @@ public class MergeIntervals {
 		// int[][] intervals = new int[][] { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 }
 		// };
 		int[][] intervals = new int[][] { { 1, 4 }, { 4, 5 } };
-		merge(intervals);
+		mergeNaive(intervals);
 	}
 
 	public static int[][] merge(int[][] intervals) {
-		List<Integer[]> ints = new ArrayList<>();
+		if (intervals.length <= 1) {
+			return intervals;
+		}
+		Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+		List<int[]> result = new ArrayList<>();
+		for (int[] interval : intervals) {
+			// if list is empty or does not overlap with the previous, just append
+			if (result.isEmpty() || result.get(result.size() - 1)[1] < interval[0]) {
+				result.add(interval);
+			} else {
+				// if overlap, merge the current and previous intervals
+				result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], interval[1]);
+			}
+		}
+		return result.toArray(new int[result.size()][]);
+	}
+
+	public static int[][] mergeNaive(int[][] intervals) {
+		if (intervals == null || intervals.length == 1) {
+			return intervals;
+		}
+		List<int[]> ints = new ArrayList<>();
 		for (int i = 0; i < intervals.length; i++) {
-			ints.add(new Integer[] { intervals[i][0], intervals[i][1] });
+			ints.add(intervals[i]);
 		}
 		boolean isOverlap = true;
 		while (isOverlap) {
 			isOverlap = false;
 			for (int i = 0; i < ints.size() - 1; i++) {
 				for (int j = i + 1; j < ints.size(); j++) {
-					Integer[] overlap = findOverlap(ints.get(i), ints.get(j));
+					int[] overlap = findOverlap(ints.get(i), ints.get(j));
 					if (overlap != null) {
 						isOverlap = true;
 						ints.remove(i);
@@ -32,14 +53,10 @@ public class MergeIntervals {
 			}
 		}
 
-		int[][] output = new int[ints.size()][2];
-		for (int i = 0; i < ints.size(); i++) {
-			output[i] = new int[] { ints.get(i)[0], ints.get(i)[1] };
-		}
-		return output;
+		return ints.toArray(new int[0][]);
 	}
 
-	public static Integer[] findOverlap(Integer[] interval1, Integer[] interval2) {
+	public static int[] findOverlap(int[] interval1, int[] interval2) {
 		int start1 = interval1[0];
 		int end1 = interval1[1];
 		int start2 = interval2[0];
@@ -50,16 +67,16 @@ public class MergeIntervals {
 		}
 		if (start1 == start2) {
 			// Stars Match
-			return new Integer[] { start1, (end2 > end1) ? end2 : end1 };
+			return new int[] { start1, (end2 > end1) ? end2 : end1 };
 		}
 		if (end1 == end2) {
 			// Ends Match
-			return new Integer[] { (start1 < start2) ? start1 : start2, end2 };
+			return new int[] { (start1 < start2) ? start1 : start2, end2 };
 		}
 		if ((end1 == start2) || (end2 == start1) || (start1 > start2 && start1 < end2)
 				|| (start2 > start1 && start2 < end1) || (end1 > start2 && end1 < end2)
 				|| (end2 > start1 && end2 < end1)) {
-			return new Integer[] { (start1 < start2) ? start1 : start2, (end2 > end1) ? end2 : end1 };
+			return new int[] { (start1 < start2) ? start1 : start2, (end2 > end1) ? end2 : end1 };
 		}
 		return null;
 	}
