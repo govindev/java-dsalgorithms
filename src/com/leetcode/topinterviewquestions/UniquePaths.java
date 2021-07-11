5c01aa58513791080f26b2b82f962f70c3fce297
govindev <vgovindarajuyadav@gmail.com>
2021-07-11 15:33:04 +0530

    Unique Paths : Dynamic Programming solution

--- src/com/leetcode/topinterviewquestions/UniquePaths.java
+++ src/com/leetcode/topinterviewquestions/UniquePaths.java
@@ -1,5 +1,7 @@
 package com.leetcode.topinterviewquestions;
 
+import java.util.Arrays;
+
 public class UniquePaths {
 
 	public static void main(String[] args) {
@@ -7,10 +9,16 @@ public class UniquePaths {
 	}
 
 	public static int uniquePaths(int m, int n) {
-		return uniquePaths(m - 1, n - 1, 0, 0);
+//		return uniquePaths(m - 1, n - 1, 0, 0);
+
+		int[][] dp = new int[m][n];
+		for (int[] row : dp) {
+			Arrays.fill(row, -1);
+		}
+		return uniquePaths(m - 1, n - 1, 0, 0, dp);
 	}
 
-	public static int uniquePaths(int m, int n, int ms, int ns) {
+	public int uniquePaths(int m, int n, int ms, int ns) {
 		// Brute force , recursive solution
 		if (m == ms && n == ns) {
 			return 1;
@@ -18,9 +26,24 @@ public class UniquePaths {
 		if (ms > m || ns > n) {
 			return 0;
 		}
-		int downPaths = uniquePaths(m, n, ms + 1, ns);
-		int rightPaths = uniquePaths(m, n, ms, ns + 1);
-		return (downPaths + rightPaths);
+		// int downPaths = uniquePaths(m, n, ms+1, ns);
+		// int rightPaths = uniquePaths(m, n, ms, ns+1);
+		// return (downPaths + rightPaths);
+		return uniquePaths(m, n, ms + 1, ns) + uniquePaths(m, n, ms, ns + 1);
+	}
+
+	public static int uniquePaths(int m, int n, int ms, int ns, int[][] dp) {
+		// Brute force , recursive solution
+		if (m == ms && n == ns) {
+			return 1;
+		}
+		if (ms > m || ns > n) {
+			return 0;
+		}
+		if (dp[ms][ns] != -1)
+			return dp[ms][ns];
+
+		return dp[ms][ns] = uniquePaths(m, n, ms + 1, ns, dp) + uniquePaths(m, n, ms, ns + 1, dp);
 	}
 
 }
