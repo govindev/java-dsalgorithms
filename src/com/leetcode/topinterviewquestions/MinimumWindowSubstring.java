b0ba31a3e4a604125cbae1d801b361f01afc3a94
govindev <vgovindarajuyadav@gmail.com>
2021-07-13 00:27:13 +0530

    Minimum Window Substring - Added DP optimization

--- src/com/leetcode/topinterviewquestions/MinimumWindowSubstring.java
+++ src/com/leetcode/topinterviewquestions/MinimumWindowSubstring.java
@@ -19,12 +19,31 @@ public class MinimumWindowSubstring {
 		if (s.length() < t.length())
 			return "";
 		Map<Character, Integer> tChs = getChsMap(t);
+
+		// DP Solution
+		Map<Integer, Map<Character, Integer>> dp = new HashMap<>();
+
 		int inc = 0;
 		while (t.length() + inc <= s.length()) {
 			int start = 0, end = t.length() + inc;
 			while (start <= s.length() - (t.length() + inc)) {
 				String ss = s.substring(start, end);
-				Map<Character, Integer> ssChs = getChsMap(ss);
+
+				Map<Character, Integer> ssChs;
+				// If Map is there for the substring
+				if (dp.get(start + end - 1) != null) {
+					ssChs = dp.get(start + end - 1);
+					Character ch = ss.charAt(ss.length() - 1);
+					if (ssChs.containsKey(ch)) {
+						ssChs.put(ch, ssChs.get(ch) + 1);
+					} else {
+						ssChs.put(ch, 1);
+					}
+				} else {
+					ssChs = getChsMap(ss);
+				}
+				dp.put(start + end, ssChs);
+
 				if (areTheChsFound(ssChs, tChs)) {
 					return ss;
 				}
@@ -37,6 +56,7 @@ public class MinimumWindowSubstring {
 	}
 
 	public static Map<Character, Integer> getChsMap(String str) {
+
 		Map<Character, Integer> strChs = new HashMap<Character, Integer>();
 		for (int i = 0; i < str.length(); i++) {
 			Character ch = str.charAt(i);
