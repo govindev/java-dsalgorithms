5bcebbe4d64934fb4332132dbc9c6bb74fcf572c
govindev <vgovindarajuyadav@gmail.com>
2021-07-13 11:23:48 +0530

    Minimu Window Substring - using hashmap and sliding window

--- src/com/leetcode/topinterviewquestions/MinimumWindowSubstring.java
+++ src/com/leetcode/topinterviewquestions/MinimumWindowSubstring.java
@@ -7,14 +7,52 @@ public class MinimumWindowSubstring {
 	public static void main(String[] args) {
 		// String s = "ab";
 		// String t = "A";
-		// String s = "ADOBECODEBANC";
-		// String t = "ABC";
-		String s = "aaaaaaaaaaaabbbbbcdd";
-		String t = "abcdd";
+		String s = "ADOBECODEBANC";
+		String t = "ABC";
+		// String s = "aaaaaaaaaaaabbbbbcdd";
+		// String t = "abcdd";
 		System.out.println(minWindow(s, t));
 	}
 
 	public static String minWindow(String s, String t) {
+		if (s.length() < t.length())
+			return "";
+		Map<Character, Integer> tChs = getChsMap(t);
+		String ss = null;
+
+		Map<Character, Integer> ssChs = new HashMap<>();
+		int left = 0, right = left;
+		Character ch = s.charAt(left);
+		ssChs.put(ch, 1);
+		while (left <= right) {
+			if (areTheChsFound(ssChs, tChs)) {
+				// Found
+				if (ss == null || ss.length() > right - left) {
+					ss = s.substring(left, right + 1);
+				}
+
+				Character leftCh = s.charAt(left);
+				// Remove the left character
+				ssChs.put(leftCh, ssChs.get(leftCh) - 1);
+				// Move the left pointer
+				left++;
+			} else {
+				// Move the right pointer
+				right++;
+				if (right < s.length()) {
+					Character rightCh = s.charAt(right);
+					if (ssChs.get(rightCh) == null) {
+						ssChs.put(rightCh, 1);
+					} else {
+						ssChs.put(rightCh, ssChs.get(rightCh) + 1);
+					}
+				} else { break; }
+			}
+		}
+		return ss;
+	}
+
+	public static String minWindowDp(String s, String t) {
 		// Brute Force
 		if (s.length() < t.length())
 			return "";
