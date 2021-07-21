f98ce4b494f506b23890cc7f26135cc68481f2db
govindev <vgovindarajuyadav@gmail.com>
2021-07-21 23:46:19 +0530

    Linked List cycle

---
+++ src/com/leetcode/topinterviewquestions/LinkedListCycle.java
@@ -0,0 +1,31 @@
+package com.leetcode.topinterviewquestions;
+
+public class LinkedListCycle {
+	public boolean hasCycle(ListNode head) {
+		if (head == null)
+			return false;
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (slow != null && fast != null) {
+			if (slow == fast)
+				return true;
+			slow = slow.next;
+			if (fast.next == null)
+				return false;
+			fast = fast.next.next;
+		}
+		return false;
+	}
+
+	public boolean hasCycleUsingSet(ListNode head) {
+		// Linear space complexity
+		Set<ListNode> visited = new HashSet<>();
+		while (head != null) {
+			if (visited.contains(head))
+				return true;
+			visited.add(head);
+			head = head.next;
+		}
+		return false;
+	}
+}
