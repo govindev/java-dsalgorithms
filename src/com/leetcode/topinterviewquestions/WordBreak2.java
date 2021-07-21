1dc8dab389cd870c7f88b16666db66ae3b1a3a13
govindev <vgovindarajuyadav@gmail.com>
2021-07-21 23:31:12 +0530

    Word Break 2

--- src/com/leetcode/topinterviewquestions/WordBreak2.java
+++ src/com/leetcode/topinterviewquestions/WordBreak2.java
@@ -4,38 +4,33 @@ import java.util.*;
 
 public class WordBreak2 {
 	public static void main(String[] args) {
-		String s = "catsanddog";
-		List<String> wordDict = Arrays.asList("cat", "cats", "and", "sand", "dog");
+//		String s = "catsanddog";
+//		List<String> wordDict = Arrays.asList("cat", "cats", "and", "sand", "dog");
+
+		String s = "pineapplepenapple";
+		List<String> wordDict = Arrays.asList("apple", "pen", "applepen", "pine", "pineapple");
 		wordBreak(s, wordDict);
 	}
 
 	public static List<String> wordBreak(String s, List<String> wordDict) {
 		Set<String> set = new HashSet<>(wordDict);
 		List<String> validStrings = new ArrayList<>();
-		getValidStrings(s, set, 0, validStrings);
+		getValidStrings(s, set, 0, validStrings, "");
 		return validStrings;
 	}
 
-	public static String getValidStrings(String s, Set<String> dict, int start, List<String> validStrings) {
+	public static void getValidStrings(String s, Set<String> dict, int start, List<String> validStrings, String current) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = start; i < s.length(); i++) {
 			sb.append(s.charAt(i));
 			if (dict.contains(sb.toString())) {
 				if (i + 1 == s.length()) {
-					return sb.toString();
+					validStrings.add(current + sb.toString());
 				} else {
-					String following = getValidStrings(s, dict, i + 1, validStrings);
-					if (following != null && !following.equals("")) {
-						String formatted = sb.toString() + " " + following;
-						if (start != 0)
-							return formatted;
-						else
-							validStrings.add(formatted);
-					}
+					getValidStrings(s, dict, i+1, validStrings, current + sb.toString() + " ");
 				}
 
 			}
 		}
-		return null;
 	}
 }
