9b7acdac869ff77b733ad83f4baab0c83e201183
govindev <vgovindarajuyadav@gmail.com>
2021-07-23 23:44:31 +0530

    Maximum product subarray linear time solution

--- src/com/leetcode/topinterviewquestions/MaximumProductSubarray.java
+++ src/com/leetcode/topinterviewquestions/MaximumProductSubarray.java
@@ -2,6 +2,26 @@ package com.leetcode.topinterviewquestions;
 
 public class MaximumProductSubarray {
 	public int maxProduct(int[] nums) {
+		// This is a linear time complexity solution
+		int max = 1, min = 1, res = nums[0];
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				max = 1;
+				min = 1;
+				res = Math.max(res, 0);
+				continue;
+			}
+			int mul1 = nums[i] * max;
+			int mul2 = nums[i] * min;
+			max = Math.max(nums[i], Math.max(mul1, mul2));
+			min = Math.min(nums[i], Math.min(mul1, mul2));
+			res = Math.max(res, Math.max(max, nums[i]));
+		}
+		return res;
+	}
+
+	public int maxProductNSquare(int[] nums) {
+		// This is an order n square solution
 		int left = 0, right = 1;
 
 		int maxProduct = nums[left];
