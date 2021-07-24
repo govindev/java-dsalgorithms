1f5d2e2e15ca9bae9605e5fc86303fb2792f6585
govindev <vgovindarajuyadav@gmail.com>
2021-07-24 11:38:53 +0530

    Find Peak Element

--- src/com/leetcode/topinterviewquestions/FindPeakElement.java
+++ src/com/leetcode/topinterviewquestions/FindPeakElement.java
@@ -2,6 +2,20 @@ package com.leetcode.topinterviewquestions;
 
 public class FindPeakElement {
 	public int findPeakElement(int[] nums) {
+		// Log time
+		int left = 0, right = nums.length - 1;
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] < nums[mid + 1]) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
+	}
+
+	public int findPeakElementLinearTime(int[] nums) {
 		for (int i = 0; i < nums.length; i++) {
 			if (i - 1 < 0 && i + 1 > nums.length - 1)
 				return 0;
