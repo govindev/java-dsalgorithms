37905c595e92d5767c8525e9e6785dc4086d92dc
govindev <vgovindarajuyadav@gmail.com>
2021-08-04 22:11:52 +0530

    Factorial trailing zeroes - Efficient solution

--- src/com/leetcode/topinterviewquestions/FactorialTrailingZeroes.java
+++ src/com/leetcode/topinterviewquestions/FactorialTrailingZeroes.java
@@ -2,6 +2,13 @@ package com.leetcode.topinterviewquestions;
 
 public class FactorialTrailingZeroes {
 	public int trailingZeroes(int n) {
+		int result = 0;
+		for (int i = 5; i <= n; i = i * 5)
+			result = result + n / i;
+		return result;
+	}
+
+	public int trailingZeroesSpace(int n) {
 		int zeros = n / 5;
 		int current = 25;
 		while (n >= current) {
