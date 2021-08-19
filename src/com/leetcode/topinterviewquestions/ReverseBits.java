1c43d7afc453103298a155728ba6c8c220e94b2e
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-08-19 15:48:58 +0530

    Reverse Bits

--- src/com/leetcode/topinterviewquestions/ReverseBits.java
+++ src/com/leetcode/topinterviewquestions/ReverseBits.java
@@ -1,5 +1,18 @@
 package com.leetcode.topinterviewquestions;
 
 public class ReverseBits {
-
+	// you need treat n as an unsigned value
+	public int reverseBits(int n) {
+		int i = 0;
+		int result = 0;
+		while (i < 32) {
+			result = result << 1;
+			if ((n & 1) > 0) {
+				result = result | 1; // simply do result++;
+			}
+			n = n >> 1;
+			i++;
+		}
+		return result;
+	}
 }
