7dd90110e16b348a592dc1a42337003bfa6d19c7
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-09-10 15:15:46 +0530

    Median Of Two Sorted Arrays - Logarithmic solution

--- src/com/leetcode/topinterviewquestions/MedianOfTwoSortedArrays.java
+++ src/com/leetcode/topinterviewquestions/MedianOfTwoSortedArrays.java
@@ -4,10 +4,10 @@ public class MedianOfTwoSortedArrays {
 	public static void main(String[] args) {
 		int[] nums1 = { 1, 2 };
 		int[] nums2 = { 3, 4 };
-		System.out.println(findMedianSortedArraysEfficeint(nums1, nums2));
+		System.out.println(findMedianSortedArraysBestOptimum(nums1, nums2));
 	}
 
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	public static double findMedianSortedArraysNaive(int[] nums1, int[] nums2) {
 		int[] temp = new int[nums1.length + nums2.length];
 		int i = 0, j = 0, k = 0;
 		while (i < nums1.length && j < nums2.length) {
@@ -85,5 +85,35 @@ public class MedianOfTwoSortedArrays {
 			return mid1value;
 		}
 	}
-	
+
+	public static double findMedianSortedArraysBestOptimum(int[] nums1, int[] nums2) {
+		if (nums1.length > nums2.length)
+			return findMedianSortedArraysBestOptimum(nums2, nums1);
+		int n1 = nums1.length;
+		int n2 = nums2.length;
+		int low = 0, high = n1;
+
+		while (low <= high) {
+			int cut1 = (low + high) / 2;
+			int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+			int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+			int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+			int right1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+			int right2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+			if (left1 <= right2 && left2 <= right1) {
+				if ((n1 + n2) % 2 == 0) {
+					return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+				}
+				return Math.max(left1, left2);
+			} else if (left1 > right2) {
+				high = cut1 - 1;
+			} else {
+				low = cut1 + 1;
+			}
+		}
+		return 0.0;
+	}
+
 }
