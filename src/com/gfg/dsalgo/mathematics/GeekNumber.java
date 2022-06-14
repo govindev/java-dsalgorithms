0d861d42315db2b5eaf578c180ac59a8f70ac53e
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-06-14 21:29:01 +0530

    geek number

--- src/com/gfg/dsalgo/mathematics/GeekNumber.java
+++ src/com/gfg/dsalgo/mathematics/GeekNumber.java
@@ -2,10 +2,29 @@ package com.gfg.dsalgo.mathematics;
 
 public class GeekNumber {
     static int geekNumber(int number){
-        for (int i = 2; i*i <= number && number > 1; i++) {
-            int power = 0;
+        if (number <= 1) return 1;
+        int power = 0;
+        while (number%2 == 0) {
+            number = number/2;
+            power++;
+            if (power > 1) return 0;
+        }
+        power = 0;
+        while (number%3 == 0) {
+            number = number/3;
+            power++;
+            if (power > 1) return 0;
+        }
+        for (int i = 5; i*i <= number; i=i+6) {
+            power = 0;
             while (number%i == 0) {
                 number = number/i;
+                power++;
+                if (power > 1) return 0;
+            }
+            power = 0;
+            while (number%(i+2) == 0) {
+                number = number/(i+2);
                 power++;
                 if (power > 1) return 0;
             }
