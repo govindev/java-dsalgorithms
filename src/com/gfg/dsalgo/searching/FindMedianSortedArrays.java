066823835c3e78fb0fc5afbf5cbfbee51c4a1347
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-06-17 01:13:40 +0530

    find median using binary search

--- src/com/gfg/dsalgo/searching/FindMedianSortedArrays.java
+++ src/com/gfg/dsalgo/searching/FindMedianSortedArrays.java
@@ -1,7 +1,13 @@
 package com.gfg.dsalgo.searching;
 
 public class FindMedianSortedArrays {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1,2};
+        int[] arr2 = new int[]{3,4};
+        findMedianSortedArrays(arr1, arr2);
+    }
+
+    public double findMedianSortedArraysNaive(int[] nums1, int[] nums2) {
         // Naive approach
         // Taking another array
         int[] nums = new int[nums1.length+nums2.length];
@@ -28,4 +34,37 @@ public class FindMedianSortedArrays {
         }
         return nums[mid];
     }
+
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+
+        int left = 0, right = n1;
+        while (left < right) {
+            int p1 = (left+right)/2;
+            int p2 = ((n1+n2+1)/2) - p1;
+
+            int v1r = (p1 == n1) ? Integer.MAX_VALUE : nums1[p1];
+            int v1l = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1-1];
+
+            int v2r = (p2 == n2) ? Integer.MAX_VALUE : nums2[p2];
+            int v2l = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2-1];
+
+            if (v1l <= v2r && v2l <= v1r) {
+                if ((n1+n2) % 2 == 0) {
+                    return (double) (Math.max(v1l, v2l) + Math.min(v1r, v2r)) / 2;
+                }
+                return (double) Math.max(v1l, v2l);
+            } else if (v1l > v2r) {
+                right  = p1-1;
+            } else {
+                left = p1+1;
+            }
+        }
+        return -1;
+    }
+
 }
