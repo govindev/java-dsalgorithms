dffd4de2d8f398cf2ecc70d1a6ac6e7f708b9ca5
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-02 05:44:19 +0530

    max circular subarray sum

--- src/com/miscellaneous/MaxSumCircularSubArray.java
+++ src/com/miscellaneous/MaxSumCircularSubArray.java
@@ -5,7 +5,34 @@ public class MaxSumCircularSubArray {
         int[] nums = {5, -3, 5};
         System.out.println("max sum : "+ maxSubarraySumCircular(nums));
     }
-    public static int maxSubarraySumCircular(int[] nums) {
+
+    public int maxSum(int[] nums) {
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currSum = (nums[i] > nums[i]+currSum) ? nums[i] : currSum + nums[i];
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+
+    public int maxSubarraySumCircular(int[] nums) {
+        int maxSum = maxSum(nums);
+
+        if (maxSum < 0) return maxSum;
+
+        int totalSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+            nums[i] = -nums[i];
+        }
+
+        int minSum = maxSum(nums);
+
+        return Math.max(maxSum, totalSum + minSum);
+    }
+
+    public static int maxSubarraySumCircularNaive(int[] nums) {
         // Brute force
         int maxSum = Integer.MIN_VALUE;
         int len = nums.length;
