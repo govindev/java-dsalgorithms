fbedfcd57b2f213468c15cb5244f593439d8e469
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-10 05:27:46 +0530

    Max consecutive ones

---
+++ src/com/miscellaneous/MaxConsecutiveOnes.java
@@ -0,0 +1,16 @@
+package com.miscellaneous;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0, maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxCount = Math.max(count, maxCount);
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
