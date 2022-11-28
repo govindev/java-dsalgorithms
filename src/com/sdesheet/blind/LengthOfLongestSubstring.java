4fad381ec45acb5cdbf43b967c100affae7db20d
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-28 07:13:58 +0530

    length of a longest substring without repeating characters

---
+++ src/com/sdesheet/blind/LengthOfLongestSubstring.java
@@ -0,0 +1,22 @@
+package com.sdesheet.blind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> lastOccs = new HashMap<>();
+        int left = 0, right = 0, max = 0;
+        while (right < s.length()) {
+            Character ch = s.charAt(right);
+            if (lastOccs.getOrDefault(ch, -1) >= left) {
+                left = lastOccs.get(ch) + 1;
+                lastOccs.remove(ch);
+            }
+            lastOccs.put(ch, right);
+            max = Math.max((right-left)+1, max);
+            right++;
+        }
+        return max;
+    }
+}
