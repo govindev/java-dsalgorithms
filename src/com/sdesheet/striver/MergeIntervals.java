f678afa1404480e117250cc51cff5106dfd628fa
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-12-02 07:20:53 +0530

    merge intervals

---
+++ src/com/sdesheet/striver/MergeIntervals.java
@@ -0,0 +1,28 @@
+package com.sdesheet.striver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <=1)return intervals;
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (end > intervals[i][0]) {
+                end = Math.max(end, intervals[i][1]);
+            } else {
+                res.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        res.add(new int[] {start, end});
+        return res.toArray(new int[0][]);
+    }
+}
