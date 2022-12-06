ed7e697de95de461ec73ddb3c869e2698462c8b2
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-12-06 19:51:10 +0530

    sub array with 0 sum

---
+++ src/com/miscellaneous/SubarrayWithZeroSum.java
@@ -0,0 +1,16 @@
+package com.miscellaneous;
+
+public class SubarrayWithZeroSum {
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n)
+    {
+        Set<Integer> sums = new HashSet<>();
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+            if (num == 0 || sum == 0 || sums.contains(sum)) return true;
+            sums.add(sum);
+        }
+        return false;
+    }
+}
