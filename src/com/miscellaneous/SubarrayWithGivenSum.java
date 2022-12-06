5508632e0b09f7643aeb7b0318b0d369adfa2df4
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-12-06 21:17:44 +0530

    sub array with given sum

---
+++ src/com/miscellaneous/SubarrayWithGivenSum.java
@@ -0,0 +1,38 @@
+package com.miscellaneous;
+
+import java.util.ArrayList;
+
+public class SubarrayWithGivenSum {
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s)
+    {
+        int start = 0, sum = arr[start];
+        if (sum == s) {
+            return res(start, start);
+        }
+        for (int end = 1; end < n; end++) {
+            if (s == arr[end]) {
+                return res(end, end);
+            } else if (s < arr[end]) {
+                start = end+1;
+                sum = 0;
+            }
+            sum += arr[end];
+            if (sum == s) return res(start, end);
+            while (sum > s && start < end) {
+                sum -= arr[start];
+                start++;
+                if (sum == s) return res(start, end);
+            }
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(-1);
+        return res;
+    }
+
+    static ArrayList<Integer> res(int start, int end) {
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(start+1);
+        res.add(end+1);
+        return res;
+    }
+}
