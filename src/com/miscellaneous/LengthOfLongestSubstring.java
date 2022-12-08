4490a7b425a24d7a7ae9cb6b41c429f60e01a4b5
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-12-08 05:55:27 +0530

    legnth of longest substring

---
+++ src/com/miscellaneous/LengthOfLongestSubstring.java
@@ -0,0 +1,22 @@
+package com.miscellaneous;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> lastOccurances = new HashMap<>();
+
+        int left = 0, right = 0, longest = 0;
+        while (right < s.length()) {
+            Character ch = s.charAt(right);
+
+            if (lastOccurances.getOrDefault(ch, -1) >= left) {
+                left = lastOccurances.get(ch) + 1;
+                lastOccurances.remove(ch);
+            }
+
+            lastOccurances.put(ch, right);
+            longest = Math.max(longest, (right-left)+1);
+            right++;
+        }
+        return longest;
+    }
+}
