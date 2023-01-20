6bee03fdfefb59a11e28ba4f6197ae39c6b0fa8e
Vurukuti Govinda Raju <vgovindev@gmail.com>
2023-01-20 15:29:30 +0530

    reverse a linked list

---
+++ src/com/miscellaneous/ReverseALinkedList.java
@@ -0,0 +1,14 @@
+package com.miscellaneous;
+
+public class ReverseALinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head, prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
