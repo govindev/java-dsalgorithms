07b5d61f48bccdcbba6821a0cf30612e113d1c82
Vurukuti Govinda Raju <vgovindev@gmail.com>
2023-01-25 01:25:46 +0530

    two sum ||

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -53,8 +53,6 @@ X
 (src/com/miscellaneous/ReverseString.java,3/8/38dbd324d54596e43a3df574e83a4edd1025050d
 X
 (src/com/miscellaneous/SecondLargest.java,2/4/246fc5fd1c6fb2e16aa8ceb3967cb154b6bcf7eb
-R
-"src/com/miscellaneous/TwoSum2.java,e/c/ecc303e5f76a26a231ceb355b586e87aa077cddd
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- src/com/miscellaneous/TwoSum2.java
+++ src/com/miscellaneous/TwoSum2.java
@@ -2,10 +2,12 @@ package com.miscellaneous;
 
 public class TwoSum2 {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0, right = numbers.length-1;
+        // Use the two pointer approach
+        int left = 0, right = numbers.length - 1;
         while (left < right) {
-            if (numbers[left]+numbers[right] == target) return new int[] {left+1, right+1};
-            else if (numbers[left]+numbers[right] > target) right--;
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) return new int[] {left+1, right+1};
+            else if (sum > target) right--;
             else left++;
         }
         return null;
