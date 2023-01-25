fc25e1ee402629625c2a3f0e264b1a524ce7f3ae
Vurukuti Govinda Raju <vgovindev@gmail.com>
2023-01-26 00:56:53 +0530

    new : subsets ||

--- src/com/sdesheet/striver/Subsets2.java
+++ src/com/sdesheet/striver/Subsets2.java
@@ -16,16 +16,31 @@ public class Subsets2 {
             subsets.add(List.of(nums[i]));
             subsets = getSubsets(nums, i+1, List.of(nums[i]), subsets);
         }
-        return subsets;
+        // return subsets;
+
+        // Method2
+        List<List<Integer>> ansList = new ArrayList<>();
+        findSubsets(0, nums, new ArrayList<>(), ansList);
+        return ansList;
     }
+
+    public void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
+        ansList.add(new ArrayList<>(ds));
+        for (int i = ind; i < nums.length; i++) {
+            if (i != ind && nums[i] == nums[i-1]) continue;
+            ds.add(nums[i]);
+            findSubsets(i+1, nums, ds, ansList);
+            ds.remove(ds.size()-1);
+        }
+    }
+
 
     public List<List<Integer>> getSubsets(int[] nums, int from, List<Integer> subset, List<List<Integer>> subsets) {
         for (int i = from; i < nums.length; i++) {
             if (i != from && nums[i] == nums[i-1]) continue;
 
-            List<Integer> list = new ArrayList<>();
+            List<Integer> list = new ArrayList<>(subset);
             list.add(nums[i]);
-            list.addAll(subset);
             subsets.add(list);
 
             subsets = getSubsets(nums, i+1, list, subsets);
