66b8469ed9fcfc25590fdfbd46f847d393fcaef6
Vurukuti Govinda Raju <vgovindev@gmail.com>
2023-01-26 23:39:15 +0530

    new : kth permutation sequence

--- src/com/sdesheet/striver/KthPermutationSequence.java
+++ src/com/sdesheet/striver/KthPermutationSequence.java
@@ -5,6 +5,27 @@ import java.util.List;
 
 public class KthPermutationSequence {
     public String getPermutation(int n, int k) {
+        k = k-1;
+        String ans = "";
+        int fact = 1;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            numbers.add(i);
+            fact *= i;
+        }
+        numbers.add(n);
+
+        while (true) {
+            ans = ans + numbers.get(k / fact);
+            numbers.remove(k / fact);
+            if (numbers.size() == 0) break;
+
+            k %= fact;
+            fact = fact / numbers.size();
+        }
+        return ans;
+    }
+    public String getPermutationBruteForce(int n, int k) {
         List<Integer> nums = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             nums.add(i);
