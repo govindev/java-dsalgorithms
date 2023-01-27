ed55cfd013f36d472336b21f83022e9d893576d3
Vurukuti Govinda Raju <vgovindev@gmail.com>
2023-01-27 13:50:02 +0530

    new : permutations approach2

--- src/com/sdesheet/neetcode/Permutations.java
+++ src/com/sdesheet/neetcode/Permutations.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteApproach1(int[] nums) {
         List<List<Integer>> permutations = new ArrayList<>();
         boolean[] used = new boolean[nums.length];
         func(nums, used, new ArrayList<>(), permutations);
@@ -26,5 +26,37 @@ public class Permutations {
             permutation.remove(permutation.size() - 1);
             used[i] = false;
         }
+    }
+
+
+
+    public List<List<Integer>> permuteApproach2(int[] nums) {
+        // Better space complexity
+        List<List<Integer>> permutations = new ArrayList<>();
+        func(nums, 0, permutations);
+        return permutations;
+    }
+
+    private void func(int[] nums, int ind, List<List<Integer>> permutations) {
+        if (ind == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            permutations.add(permutation);
+            return;
+        }
+
+        for (int i = ind; i < nums.length; i++) {
+            swap(i, ind, nums);
+            func(nums, ind+1, permutations);
+            swap(i, ind, nums);
+        }
+    }
+
+    private void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
