987980113fc2b500dd888c4fd1052ba7bca05cc6
Vurukuti Govinda Raju <vgovindev@gmail.com>
2023-02-20 18:57:52 +0530

    find duplicate

--- src/com/miscellaneous/FindDuplicate.java
+++ src/com/miscellaneous/FindDuplicate.java
@@ -2,11 +2,14 @@ package com.miscellaneous;
 
 public class FindDuplicate {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0], fast = nums[0];
+        // Using the slow and fast pointer approach
+        int slow = nums[0];
+        int fast = nums[0];
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
+
         fast = nums[0];
         while (slow != fast) {
             slow = nums[slow];
