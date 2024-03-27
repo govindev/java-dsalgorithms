cb83bc79eecb2ef657315814deecf5e3040f4623
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2024-03-27 22:07:36 +0530

    search in rotated sorted array

---
+++ src/com/miscellaneous/SearchInRoatedSortedArray.java
@@ -0,0 +1,22 @@
+package com.miscellaneous;
+
+public class SearchInRoatedSortedArray {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[low] <= nums[mid]) {
+                // left half is sorted
+                if (target >= nums[low] && target < nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else {
+                // righ half is sorted
+                if (target >= nums[mid] && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
