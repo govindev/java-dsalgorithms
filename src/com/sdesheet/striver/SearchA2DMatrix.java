b0854580f14666e7fb4d71580ce508271739e332
govindev <vgovindev@gmail.com>
2024-08-17 12:33:35 +0530

    search 2d matrix

--- src/com/sdesheet/striver/SearchA2DMatrix.java
+++ src/com/sdesheet/striver/SearchA2DMatrix.java
@@ -2,10 +2,10 @@ package com.sdesheet.striver;
 
 public class SearchA2DMatrix {
     public static void main(String[] args) {
-//        int[][] matrix = new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        int[][] matrix = new int[][] {{1,3}};
+        int[][] matrix = new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+//        int[][] matrix = new int[][] {{1,3}};
         int target = 3;
-        System.out.println(searchMatrix(matrix, target));
+        System.out.println(search2DMatrix(matrix, target));
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
@@ -30,6 +30,31 @@ public class SearchA2DMatrix {
                 else l = rm + 1;
             }
             return false;
+        }
+        return false;
+    }
+
+
+    public static boolean search2DMatrix(int[][] matrix, int target) {
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
+
+        int top = 0; int bottom = rowLen - 1;
+        while (top <= bottom) {
+            int center = (top+bottom) / 2;
+            if (target >= matrix[center][0] && target <= matrix[center][colLen-1]) {
+                int start = 0;
+                int end = colLen-1;
+                while (start <= end) {
+                    int mid = (start+end) / 2;
+                    if (matrix[center][mid] == target) return true;
+                    else if (matrix[center][mid] > target) end = mid-1;
+                    else start = mid+1;
+                }
+                return false;
+            }
+            else if (target < matrix[center][0]) bottom = center - 1;
+            else top = center + 1;
         }
         return false;
     }
