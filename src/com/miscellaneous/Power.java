c427322484e7ce214f982c0f0870d9ec43e8fac8
govindev <vgovindev@gmail.com>
2024-08-17 11:48:43 +0530

    power

--- src/com/miscellaneous/Power.java
+++ src/com/miscellaneous/Power.java
@@ -2,16 +2,18 @@ package com.miscellaneous;
 
 public class Power {
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (n == 1) return x;
         if (n < 0) {
-            n = (n == Integer.MIN_VALUE) ? Integer.MAX_VALUE-1 : -n;
-            return 1/myPow(x, n);
-        } else if (n%2 == 0) {
-            double pow = myPow(x, n/2);
-            return pow * pow;
-        } else {
-            return myPow(x, n-1) * x;
+            n = (n == Integer.MIN_VALUE) ? Integer.MAX_VALUE - 1 : (-1)*n;
+            return 1 / myPow(x, n);
         }
+        if (n == 0) return 1;
+
+        if (n == 1) return x;
+
+        if (n%2 == 0) {
+            double half = myPow(x, n/2);
+            return half * half;
+        }
+        return myPow(x, n-1) * x;
     }
 }
