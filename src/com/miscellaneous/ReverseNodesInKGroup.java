80bb9f1e66612c13b4b0f707c486bc10d375e9c6
govindev <vgovindev@gmail.com>
2024-08-20 06:06:59 +0530

    update reverse k nodes in a linked list

--- src/com/miscellaneous/ReverseNodesInKGroup.java
+++ src/com/miscellaneous/ReverseNodesInKGroup.java
@@ -2,32 +2,30 @@ package com.miscellaneous;
 
 public class ReverseNodesInKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-        while (true) {
-            // Check if elements count is greater than or equal to k
-            int tmpK = k;
-            ListNode temp = head;
-            while (k > 0 && temp != null) {
-                temp = temp.next;
-                k--;
-            }
-            // This means elements count is less than k
-            if (k > 0) return head;
-
-            // Reset k
-            k = tmpK;
-
-            ListNode current = head, prev = null;
-            while (k > 0) {
-                temp = current.next;
-                current.next = prev;
-                prev = current;
-                current = temp;
-                k--;
-            }
-            // After the reversal head is the last element, current is the next group start
-            // and prev is the start of the current group
-            head.next = reverseKGroup(current, tmpK);
-            return prev;
+        // Check if elements count is greater than or equal to k
+        int tmpK = k;
+        ListNode temp = head;
+        while (k > 0 && temp != null) {
+            temp = temp.next;
+            k--;
         }
+        // This means elements count is less than k
+        if (k > 0) return head;
+
+        // Reset k
+        k = tmpK;
+
+        ListNode current = head, prev = null;
+        while (k > 0) {
+            temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+            k--;
+        }
+        // After the reversal head is the last element, current is the next group start
+        // and prev is the start of the current group
+        head.next = reverseKGroup(current, tmpK);
+        return prev;
     }
 }
