14da3899de3dc0bafc051bf4c7b970d8d9c9c069
govindev <vgovindev@gmail.com>
2024-08-25 12:10:43 +0530

    majority element by 3

--- src/com/miscellaneous/MajorityElementBy3.java
+++ src/com/miscellaneous/MajorityElementBy3.java
@@ -1,12 +1,18 @@
 package com.miscellaneous;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MajorityElementBy3 {
-    public List<Integer> majorityElement(int[] nums) {
+    public static List<Integer> majorityElement(int[] nums) {
+        if (nums.length == 0) return Collections.emptyList();
+        if (nums.length == 1) return List.of(nums[0]);
+
+        List<Integer> majorities = new ArrayList<>();
+
         int majority1 = -1, majority2 = -1, count1 = 0, count2 = 0;
-        for (int num : nums) {
+        for (int num: nums) {
             if (majority1 == num) count1++;
             else if (majority2 == num) count2++;
             else if (count1 == 0) {
@@ -21,15 +27,21 @@ public class MajorityElementBy3 {
                 count1--; count2--;
             }
         }
-
-        List<Integer> majorities = new ArrayList<>();
         count1 = 0; count2 = 0;
         for (int num : nums) {
-            if (num == majority1) count1++;
-            else if (num == majority2) count2++;
+            if (majority1 == num) count1++;
+            else if (majority2 == num) count2++;
         }
+
         if (count1 > Math.ceil(nums.length/3)) majorities.add(majority1);
         if (count2 > Math.ceil(nums.length/3)) majorities.add(majority2);
+
         return majorities;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {3,2,3};
+        List<Integer> majorities = majorityElement(nums);
+        majorities.forEach(majority -> System.out.println(majority + ","));
     }
 }
