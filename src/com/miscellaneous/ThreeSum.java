7b06d665a357496abab5dfd57da0c7bc9b550ddf
govindev <vgovindev@gmail.com>
2024-08-26 09:04:41 +0530

    three sum

--- src/com/miscellaneous/ThreeSum.java
+++ src/com/miscellaneous/ThreeSum.java
@@ -32,4 +32,41 @@ public class ThreeSum {
         }
         return triplets;
     }
+
+    public List<List<Integer>> threeSumNew(int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1, right = nums.length - 1;
+            while (left < right) {
+                if (left > i+1 && left < nums.length - 1 && nums[left] == nums[left-1]) {
+                    left++;
+                    continue;
+                }
+                if (right > 0 && right < nums.length - 1 && nums[right] == nums[right+1]) {
+                    right--;
+                    continue;
+                }
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triplets.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
+                    // while (left < nums.length - 1 && nums[left] == nums[left-1]) left++;
+                    right--;
+                    // while (right > 0 && nums[right] == nums[right+1]) right--;
+                }
+                else if (sum > 0) {
+                    right--;
+                    // while (right > 0 && nums[right] == nums[right+1]) right--;
+                }
+                else {
+                    left++;
+                    // while (left < nums.length - 1 && nums[left] == nums[left-1]) left++;
+                }
+            }
+        }
+        return triplets;
+    }
 }
