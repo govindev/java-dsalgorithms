98c38a9b69188cf0cf54f30f7d84a244edeeed77
govindev <vgovindev@gmail.com>
2024-08-27 08:19:13 +0530

    four sum

--- src/com/miscellaneous/FourSum.java
+++ src/com/miscellaneous/FourSum.java
@@ -7,37 +7,30 @@ import java.util.List;
 public class FourSum {
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        // 1. First do the sorting
+        List<List<Integer>> quadruplets = new ArrayList<>();
+
         Arrays.sort(nums);
 
+        for (int i = 0; i < nums.length-3; i++) {
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < nums.length - 2; j++) {
+                if (j != i+1 && nums[j] == nums[j-1]) continue;
+                int left = j+1, right = nums.length - 1;
+                while (left < right) {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum > target) right--;
+                    else if (sum < target) left++;
+                    else {
+                        quadruplets.add(List.of(nums[i], nums[j], nums[left], nums[right]));
 
-        // 2. Create the list
-        List<List<Integer>> res = new ArrayList<>();
-
-        for (int a = 0; a < nums.length - 3; a++) {
-
-            if (a != 0 && nums[a-1] == nums[a]) continue;
-
-            for (int b = a+1; b < nums.length - 2; b++) {
-
-                if (b != a+1 && nums[b-1] == nums[b]) continue;
-                int c = b+1, d = nums.length-1;
-
-                while (c < d)  {
-                    int sum = nums[a] + nums[b] + nums[c] + nums[d];
-                    if (sum > target) {
-                        d--;
-                    } else if (sum < target) {
-                        c++;
-                    } else {
-                        res.add(List.of(nums[a], nums[b], nums[c], nums[d]));
-                        c++;
-                        while (c < d && nums[c] == nums[c-1]) c++;
+                        right--;
+                        left++;
+                        while (left < right && nums[right] == nums[right+1]) right--;
+                        while (left < right && nums[left] == nums[left-1]) left++;
                     }
                 }
             }
         }
-
-        return res;
+        return quadruplets;
     }
 }
