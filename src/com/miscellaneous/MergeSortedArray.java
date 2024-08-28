15510b7aa7ab1a8751d05afe1fb4e78f5f58d7c5
govindev <vgovindev@gmail.com>
2024-08-28 11:20:28 +0530

    merge sorted array

--- src/com/miscellaneous/MergeSortedArray.java
+++ src/com/miscellaneous/MergeSortedArray.java
@@ -2,39 +2,43 @@ package com.miscellaneous;
 
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1, j = n-1, k = nums1.length - 1;
-        while (k >= 0) {
-            if ((i >= 0 && j >= 0 && nums1[i] > nums2[j])
-                    || (i >= 0 && j < 0)) {
-                nums1[k] = nums1[i];
-                i--;
-                k--;
-            } else {
-                nums1[k] = nums2[j];
-                j--;
-                k--;
+        int curr = m+n-1;
+        int p1 = m-1, p2 = n-1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[curr--] = nums1[p1--];
             }
+            else {
+                nums1[curr--] = nums2[p2--];
+            }
+        }
+        while (p1 >= 0) {
+            nums1[curr--] = nums1[p1--];
+        }
+        while (p2 >= 0) {
+            nums1[curr--] = nums2[p2--];
         }
     }
 
-    public void merge1(int[] nums1, int m, int[] nums2, int n) {
-        // Idea is to start from the end and move towards left
-        int p1 = m - 1, p2 = n - 1;
-        for (int i = nums1.length - 1; i >= 0; i--) {
-            if (p1 < 0 && p2 < 0) return;
-            else if (p1 < 0) {
-                nums1[i] = nums2[p2];
-                p2--;
-            } else if (p2 < 0) {
-                nums1[i] = nums1[p1];
-                p1--;
-            } else if (nums2[p2] > nums1[p1]) {
-                nums1[i] = nums2[p2];
-                p2--;
-            } else {
-                nums1[i] = nums1[p1];
-                p1--;
+    public void mergeExtraSpace(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m+n];
+        int i = 0, j = 0, l = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp[l++] = nums1[i++];
             }
+            else {
+                temp[l++] = nums2[j++];
+            }
+        }
+        while (i < m) {
+            temp[l++] = nums1[i++];
+        }
+        while (j < n) {
+            temp[l++] = nums2[j++];
+        }
+        for (int k = 0; k < temp.length; k++) {
+            nums1[k] = temp[k];
         }
     }
 }
