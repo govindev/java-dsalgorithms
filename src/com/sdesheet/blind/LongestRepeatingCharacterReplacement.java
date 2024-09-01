8af10e8f5b1dcf6dbf83d389dd2023e1cbb443fa
govindev <vgovindev@gmail.com>
2024-09-01 14:23:14 +0530

    longest repeating character replacement

--- src/com/sdesheet/blind/LongestRepeatingCharacterReplacement.java
+++ src/com/sdesheet/blind/LongestRepeatingCharacterReplacement.java
@@ -10,28 +10,45 @@ public class LongestRepeatingCharacterReplacement {
         int k = 1;
         System.out.println(characterReplacement(s, k));
     }
-
-    public static int characterReplacement(String s, int k) {
-        int left = 0, right = 0, res = 0;
-        Map<Character, Integer> chCounts = new HashMap<>();
-        while (right < s.length()) {
-            Character curr = s.charAt(right);
-            chCounts.put(curr, chCounts.getOrDefault(curr, 0)+1);
-
-            int maxFreq = 0;
-            for (Integer count : chCounts.values()) {
-                maxFreq = Math.max(maxFreq, count);
+    public int characterReplacementPartial(String s, int k) {
+        int longest = (s.length() == k) ? k : k+1;
+        for (int i = 0; i < s.length(); i++) {
+            int currK = k;
+            int j = i+1;
+            for (; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    continue;
+                }
+                if (currK == 0) {
+                    break;
+                }
+                currK--;
             }
-
-            int winLen = (right - left) + 1;
-            if (winLen-maxFreq <= k) {
-                res = Math.max(res, winLen);
-            } else {
-                chCounts.put(s.charAt(left), chCounts.get(s.charAt(left))-1);
-                left++;
-            }
-            right++;
+            longest = Math.max(longest, j-i);
         }
-        return res;
+        return longest;
+    }
+
+    public int characterReplacement(String s, int k) {
+        int[] characterCounts = new int[26];
+        int start = 0, end = 0, maxRepChCount = 0, maxWindowLength = 0;
+        while (start <= end && end < s.length()) {
+            characterCounts[s.charAt(end) - 'A']++;
+            maxRepChCount = Math.max(maxRepChCount, characterCounts[s.charAt(end)-'A']);
+
+            int windowLength = end-start+1;
+            if (windowLength - maxRepChCount <= k) {
+                maxWindowLength = Math.max(maxWindowLength, windowLength);
+            } else {
+                // If the last substring is not a valid string
+                // Then move the left side of the window
+                characterCounts[s.charAt(start) - 'A']--;
+                start++;
+            }
+            end++;
+        }
+
+        return maxWindowLength;
     }
 }
+
