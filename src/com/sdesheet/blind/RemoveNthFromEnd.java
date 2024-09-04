938f875dbe31002b1091572c0f0634ee4528ff41
govindev <vgovindev@gmail.com>
2024-09-04 19:42:17 +0530

    remove nth node from end of list

--- src/com/sdesheet/blind/RemoveNthFromEnd.java
+++ src/com/sdesheet/blind/RemoveNthFromEnd.java
@@ -2,24 +2,35 @@ package com.sdesheet.blind;
 
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode curr = head;
-        int length = 0;
-        while (curr != null) {
-            length++;
-            curr = curr.next;
-        }
-        // 5 - 2
-        int i = 0;
-        ListNode headsHead = new ListNode(0, head);
-        curr = headsHead;
-        while (curr != null) {
-            if (length-n == i) {
-                // Remove the next node
-                curr.next = curr.next.next;
-                return headsHead.next;
+        int length = countNumberOfNodes(head);
+        int deleteIndex = relativeZeroIndexStartNumberForLast(n, length);
+
+        ListNode startNode = new ListNode(0, head), prevNode = startNode;
+        ListNode currentNode = head;
+        int index = 0;
+        while (currentNode != null) {
+            if (index == deleteIndex) {
+                prevNode.next = currentNode.next;
+                break;
             }
-            curr = curr.next; i++;
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            index++;
         }
-        return null;
+
+        return startNode.next;
+    }
+
+    private int countNumberOfNodes(ListNode head) {
+        int numberOfNodes = 0;
+        while (head != null) {
+            head = head.next;
+            numberOfNodes++;
+        }
+        return numberOfNodes;
+    }
+
+    private int relativeZeroIndexStartNumberForLast(int last, int length) {
+        return length - last;
     }
 }
