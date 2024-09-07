823a68c173c22c83eac4101557d89c6df17cd2a5
govindev <vgovindev@gmail.com>
2024-09-07 13:27:20 +0530

    contains duplicate

--- src/com/sdesheet/blind/ContainsDuplicate.java
+++ src/com/sdesheet/blind/ContainsDuplicate.java
@@ -1,15 +1,50 @@
 package com.sdesheet.blind;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        // 3 solutions
+
+        // 1. order of n square. Two loops
+        // return containsDuplicateWithoutExtraSpace(nums);
+
+        // 2. order of logn do the sort
+        // return containsDuplicateUsingSort(nums);
+
+        // 3. order of 1 have the set
+        return containsDuplicateUsingSet(nums);
+
+    }
+
+    public boolean containsDuplicateWithoutExtraSpace(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) return true;
-            set.add(nums[i]);
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
         }
+
+        return false;
+    }
+
+    public boolean containsDuplicateUsingSort(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) return true;
+        }
+
+        return false;
+    }
+
+    public boolean containsDuplicateUsingSet(int[] nums) {
+        Set<Integer> uniqueNums = new HashSet<>();
+        for (int num : nums) {
+            if (uniqueNums.contains(num)) return true;
+            uniqueNums.add(num);
+        }
+
         return false;
     }
 }
