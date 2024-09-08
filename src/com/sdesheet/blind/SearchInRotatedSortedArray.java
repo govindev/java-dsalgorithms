e47e381265cb52d6b173be6850113fb73d5862f4
govindev <vgovindev@gmail.com>
2024-09-08 12:56:04 +0530

    search in rotated sorted array

--- src/com/sdesheet/blind/SearchInRotatedSortedArray.java
+++ src/com/sdesheet/blind/SearchInRotatedSortedArray.java
@@ -4,22 +4,23 @@ public class SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] >= nums[left]) {
-                // Check whether left is sorted
+            int mid = (left+right)/2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            else if (nums[left] <= nums[mid]) {
+                // Left half is sorted
                 if (target >= nums[left] && target <= nums[mid]) {
-                    right = mid-1;
+                    right = mid - 1;
                 } else {
-                    left = mid+1;
+                    left = mid + 1;
                 }
             } else {
                 // Right half is sorted
-                // Check whether element is in the right half or not
                 if (target >= nums[mid] && target <= nums[right]) {
-                    left = mid+1;
+                    left = mid + 1;
                 } else {
-                    right = mid-1;
+                    right = mid - 1;
                 }
             }
         }
