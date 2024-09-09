a487a0792b421ead458212d3a455d02252c74788
govindev <vgovindev@gmail.com>
2024-09-09 19:57:15 +0530

    linkedlist cycle

--- src/com/sdesheet/blind/LinkedListCycle.java
+++ src/com/sdesheet/blind/LinkedListCycle.java
@@ -2,16 +2,15 @@ package com.sdesheet.blind;
 
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (slow != null && fast != null) {
+        ListNode slow = head, fast = head;
+        while (true) {
+            if (slow == null) return false;
             slow = slow.next;
-            fast = fast.next;
-            if (fast != null) fast = fast.next; else return false;
+
+            if (fast == null || fast.next == null) return false;
+            fast = fast.next.next;
+
             if (slow == fast) return true;
         }
-
-        return false;
     }
 }
