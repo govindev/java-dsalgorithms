106adbcc3058dd2b1a34d870aef187a3815c725f
govindev <vgovindev@gmail.com>
2024-09-14 13:34:10 +0530

    valid Parentheses

---
+++ src/com/sdesheet/blind/ValidParentheses.java
@@ -0,0 +1,29 @@
+package com.sdesheet.blind;
+
+import java.util.Stack;
+
+public class ValidParantheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty()) {
+                char top = (char) stack.peek();
+                if (isItCloseOfTop(top, ch)) {
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(ch);
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isItCloseOfTop(char top, char curr) {
+        return switch (top) {
+            case '[' -> curr == ']';
+            case '{' -> curr == '}';
+            case '(' -> curr == ')';
+            default -> false;
+        };
+    }
+}
