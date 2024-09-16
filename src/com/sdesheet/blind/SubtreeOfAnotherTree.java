58a9bc6d20fcbf2483d5a1b334755c7fac60d70f
govindev <vgovindev@gmail.com>
2024-09-16 11:03:05 +0530

    subtree of another tree

---
+++ src/com/sdesheet/blind/SubtreeOfAnotherTree.java
@@ -0,0 +1,18 @@
+package com.sdesheet.blind;
+
+public class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        return isSameTree(root, subRoot)
+                || isSubtree(root.left, subRoot)
+                || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
