445d6f5592fdfed61464886e70682a5b1520e6f7
govindev <vgovindev@gmail.com>
2024-09-20 22:06:17 +0530

    length of longest substring without repeating characters

--- src/com/sdesheet/blind/LengthOfLongestSubstring.java
+++ src/com/sdesheet/blind/LengthOfLongestSubstring.java
@@ -5,27 +5,26 @@ import java.util.Map;
 
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> characterCounts = new HashMap<>();
+        Map<Character, Integer> charCounts = new HashMap<>();
 
-        int left = 0, right = 0, maxLength = 0;
+        int left = 0, right = 0, longestLen = 0;
         while (left <= right && right < s.length()) {
-            char currentCh = s.charAt(right);
-            int prevCountOfChar = characterCounts.getOrDefault(currentCh, 0);
-            if (prevCountOfChar > 0) {
-                moveLeft(left, s, characterCounts);
+            char ch = s.charAt(right);
+            int charCount = charCounts.getOrDefault(ch, 0);
+            if (charCount == 1) {
+                char leftCh = s.charAt(left);
+                charCounts.put(leftCh, 0);
                 left++;
-            } else {
-                characterCounts.put(currentCh, 1);
-                maxLength = Math.max(maxLength, right - left + 1);
-                right++;
+                continue;
             }
+            charCounts.put(ch, 1);
+
+            int windowLen = (right - left) + 1;
+            longestLen = Math.max(windowLen, longestLen);
+
+            right++;
         }
 
-        return maxLength;
-    }
-
-    private void moveLeft(int left, String s, Map<Character, Integer> characterCounts) {
-        char leftCh = s.charAt(left);
-        characterCounts.put(leftCh, characterCounts.getOrDefault(leftCh, 1) - 1);
+        return longestLen;
     }
 }
