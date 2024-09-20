f1cd1e4c664957f5ff3d0f4a846af88c1769d3a5
govindev <vgovindev@gmail.com>
2024-09-20 21:05:56 +0530

    combination sum 1

--- src/com/sdesheet/blind/CombinationSum1.java
+++ src/com/sdesheet/blind/CombinationSum1.java
@@ -7,30 +7,29 @@ import java.util.List;
 public class CombinationSum1 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-
         List<List<Integer>> combinations = new ArrayList<>();
         combinationSum(candidates, target, 0, new ArrayList<>(), combinations);
-
         return combinations;
     }
 
-    private void combinationSum(int[] candidates, int target, int current,
-                                List<Integer> combination, List<List<Integer>> combinations) {
-
-        if (current == candidates.length) {
-            if (target == 0) {
-                combinations.add(new ArrayList<>(combination));
-            }
+    public void combinationSum(int[] candidates, int target, int current,
+                               List<Integer> combination, List<List<Integer>> combinations) {
+        if (target == 0) {
+            combinations.add(new ArrayList<>(combination));
+        }
+        if (current >= candidates.length) {
+            return;
+        }
+        if (candidates[current] <= target) {
+            // Pick
+            combination.add(candidates[current]);
+            combinationSum(candidates, target - candidates[current], current, combination, combinations);
+            combination.removeLast();
+        } else {
             return;
         }
 
-        if (candidates[current] <= target) {
-            combination.add(candidates[current]);
-            // See if we can pick the current element again
-            combinationSum(candidates, target-candidates[current], current, combination, combinations);
-            combination.remove(combination.size() - 1);
-        }
-
+        // Not pick
         combinationSum(candidates, target, current+1, combination, combinations);
     }
 }
--- src/com/sdesheet/blind/LongestConsecutive.java
+++ src/com/sdesheet/blind/LongestConsecutive.java
@@ -26,6 +26,7 @@ public class LongestConsecutive {
         return maxConsecutiveCount;
     }
 
+
     private Set<Integer> uniqueNums = new HashSet<>();
 
     public int longestConsecutiveBySet(int[] nums) {
