d065dc3ee28021c74775f2ce104827be209f650a
govindev <vgovindev@gmail.com>
2024-09-20 21:50:45 +0530

    longest repeating character replacement

--- src/com/sdesheet/blind/LongestRepeatingCharacterReplacement.java
+++ src/com/sdesheet/blind/LongestRepeatingCharacterReplacement.java
@@ -56,30 +56,37 @@ public class LongestRepeatingCharacterReplacement {
     // Check if the length of the substring == count of most rep ch + k
     // Modify the result if it is less than the substring length
     public static int characterReplacementUsingMap(String s, int k) {
-        if (s == null) return 0;
-        if (s.length() <= k) return s.length();
-
-        int left = 0, right = 0, maxFreq = 0, maxWindowLength = 0;
-        Map<Character, Integer> characterCounts = new HashMap<>();
-        while (right < s.length()) {
-            char ch = s.charAt(right);
-            characterCounts.put(ch, characterCounts.getOrDefault(ch, 0)+1);
-
-            maxFreq = Math.max(maxFreq, characterCounts.get(ch));
-
-            int windowLength = (right - left) + 1;
-            if (windowLength - maxFreq <= k) {
-                // Valid window
-                maxWindowLength = Math.max(maxWindowLength, windowLength);
-            } else {
-                // Move the left pointer
-                ch = s.charAt(left);
-                characterCounts.put(ch, characterCounts.get(ch) - 1);
-                left++;
+            if (s == null) {
+                return 0;
             }
-            right++;
+            if (s.length() <= k) {
+                return s.length();
+            }
+            int start = 0, end = 0, longestLen = 0, maxCharCount = 0;
+            Map<Character, Integer> charCounts = new HashMap<>();
+            while (start <= end && end < s.length()) {
+                char ch = s.charAt(end);
+                int charCount = charCounts.getOrDefault(ch, 0);
+                charCounts.put(ch, ++charCount);
+
+                int windowLen = (end - start) + 1;
+                maxCharCount = Math.max(charCount, maxCharCount);
+
+
+                if ((windowLen - maxCharCount) <= k) {
+                    // Valid window
+                    longestLen = Math.max(windowLen, longestLen);
+                } else {
+                    // Invalid window
+                    char startCh = s.charAt(start);
+                    int startCharCount = charCounts.getOrDefault(startCh, 0);
+                    charCounts.put(startCh, --startCharCount);
+                    start++;
+                }
+                end++;
+            }
+
+            return longestLen;
         }
-        return maxWindowLength;
-    }
 }
 
