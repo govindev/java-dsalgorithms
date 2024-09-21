6cce6c5e00ea2bfe62d6d315253610dddc0db869
govindev <vgovindev@gmail.com>
2024-09-21 14:16:52 +0530

    set matrix zeros

--- src/com/sdesheet/blind/SetMatrixZero.java
+++ src/com/sdesheet/blind/SetMatrixZero.java
@@ -4,47 +4,65 @@ import java.util.Arrays;
 
 public class SetMatrixZero {
     public void setZeroes(int[][] matrix) {
-        boolean isZeroCol = false, isZeroRow = false;
+        boolean isRowZeroAZeroRow = false;
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
+                    // rowMarks[i] = 0;
+                    if (i == 0) {
+                        isRowZeroAZeroRow = true;
+                    } else {
+                        matrix[i][0] = 0;
+                    }
+
+                    // colMarks[j] = 0;
                     matrix[0][j] = 0;
-                    if (j == 0) isZeroRow = true;
-                    if (i == 0) isZeroCol = true;
                 }
             }
         }
 
-        for (int i = matrix.length-1; i >= 0; i--) {
-            for (int j = matrix[i].length-1; j >= 0; j--) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    if (i != 0 && j != 0) matrix[i][j] = 0;
-                    if ((j == 0 && isZeroRow) || (i == 0 && isZeroCol)) matrix[i][j] = 0;
+                    matrix[i][j] = 0;
                 }
+            }
+        }
+
+        // For the first column
+        if (matrix[0][0] == 0) {
+            for (int i = 1; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        // For the first row
+        if (isRowZeroAZeroRow) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
-    public void setZeroes1(int[][] matrix) {
-        int[] rows = new int[matrix.length];
-        int[] cols = new int[matrix[0].length];
 
-        // Fill the rows and cols arrays with non-zero values
-        Arrays.fill(rows, 1);
-        Arrays.fill(cols, 1);
+    public void setZeroesExtraSpace(int[][] matrix) {
+        int[] rowMarks = new int[matrix.length];
+        Arrays.fill(rowMarks, -1);
+        int[] colMarks = new int[matrix[0].length];
+        Arrays.fill(colMarks, -1);
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
-                    rows[i] = 0;
-                    cols [j] = 0;
+                    rowMarks[i] = 0;
+                    colMarks[j] = 0;
                 }
             }
         }
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (rows[i] == 0 || cols[j] == 0) {
+                if (rowMarks[i] == 0 || colMarks[j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
