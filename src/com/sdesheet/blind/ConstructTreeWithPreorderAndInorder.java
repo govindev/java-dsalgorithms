0053278a4e1f7e1e38e59ef14e8918e686019f02
govindev <vgovindev@gmail.com>
2024-09-27 07:28:00 +0530

    construct binary tree from preorder and inorder traversal

--- src/com/sdesheet/blind/ConstructTreeWithPreorderAndInorder.java
+++ src/com/sdesheet/blind/ConstructTreeWithPreorderAndInorder.java
@@ -3,7 +3,35 @@ package com.sdesheet.blind;
 import java.util.Arrays;
 
 public class ConstructTreeWithPreorderAndInorder {
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // return buildTreeExtraSpace(preorder, inorder);
+        Map<Integer, Integer> elmIndexes = new HashMap<Integer, Integer>();
+        for (int i = 0; i < inorder.length; i++) {
+            elmIndexes.put(inorder[i], i);
+        }
+        return buildTree(preorder, inorder, elmIndexes, 0, preorder.length-1, 0, inorder.length-1);
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder, Map<Integer, Integer> elmIndexes,
+                              int poStart, int poEnd, int ioStart, int ioEnd) {
+        if (poStart > poEnd || ioStart > ioEnd) {
+            return null;
+        }
+        if (poStart == poEnd) {
+            return new TreeNode(preorder[poStart]);
+        }
+        int root = preorder[poStart];
+        int rootIndex = elmIndexes.get(root);
+        int numberOfElms = (rootIndex-ioStart+1);
+
+        TreeNode rootNode = new TreeNode(root);
+        rootNode.left = buildTree(preorder, inorder, elmIndexes, poStart+1, poStart+numberOfElms-1, ioStart, rootIndex-1);
+        rootNode.right = buildTree(preorder, inorder, elmIndexes, poStart+numberOfElms, poEnd, rootIndex+1, ioEnd);
+        return rootNode;
+    }
+
+    public TreeNode buildTreeExtraSpace(int[] preorder, int[] inorder) {
         if (inorder.length == 0) {
             return null;
         }
@@ -25,8 +53,8 @@ public class ConstructTreeWithPreorderAndInorder {
         int[] leftPreorder = Arrays.copyOfRange(preorder, 1, leftInorder.length+1);
         int[] rightPreorder = Arrays.copyOfRange(preorder, leftInorder.length+1, preorder.length);
 
-        rootNode.left = buildTree(leftPreorder, leftInorder);
-        rootNode.right = buildTree(rightPreorder, rightInorder);
+        rootNode.left = buildTreeExtraSpace(leftPreorder, leftInorder);
+        rootNode.right = buildTreeExtraSpace(rightPreorder, rightInorder);
 
         return rootNode;
     }
