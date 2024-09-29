7f79702e56d9ea3edd5d03a9fc7774c4e9514071
govindev <vgovindev@gmail.com>
2024-09-29 12:24:57 +0530

    find minimum in sorted array

--- src/com/sdesheet/blind/FindMinimumInRotatedSortedArray.java
+++ src/com/sdesheet/blind/FindMinimumInRotatedSortedArray.java
@@ -4,16 +4,18 @@ public class FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1, min = Integer.MAX_VALUE;
         while (left <= right) {
-            int mid = left + (right-left)/2;
-
+            int mid = left + (right-left) / 2;
+            // Check if the left is sorted
             if (nums[left] <= nums[mid]) {
-                // Left half is sorted
-                min = Math.min(min, nums[left]);
+                min = Math.min(nums[left], min);
+                // explore the right tree
                 left = mid+1;
-            } else {
-                // Right half is sorted
-                min = Math.min(min, nums[mid]);
-                right = mid-1;
+            }
+            else {
+                // Right is sorted
+                min = Math.min(nums[mid], min);
+                // Explore the left
+                right = mid - 1;
             }
         }
         return min;
