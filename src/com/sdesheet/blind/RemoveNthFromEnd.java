96290ec45bdb7ca74c52ec7b0699a8edc3ff7e66
govindev <vgovindev@gmail.com>
2024-10-05 14:09:11 +0530

    remove nth node from end of list

--- src/com/sdesheet/blind/RemoveNthFromEnd.java
+++ src/com/sdesheet/blind/RemoveNthFromEnd.java
@@ -2,35 +2,63 @@ package com.sdesheet.blind;
 
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int length = countNumberOfNodes(head);
-        int deleteIndex = relativeZeroIndexStartNumberForLast(n, length);
+        //  return removeNthFromEndNaive(head, n);
+        return removeNthNodeFromEndOnePass(head, n);
+    }
 
-        ListNode startNode = new ListNode(0, head), prevNode = startNode;
-        ListNode currentNode = head;
-        int index = 0;
-        while (currentNode != null) {
-            if (index == deleteIndex) {
-                prevNode.next = currentNode.next;
+    public ListNode removeNthNodeFromEndOnePass(ListNode head, int n) {
+        ListNode start = head;
+        ListNode end = head;
+
+        ListNode headsPrev = new ListNode();
+        headsPrev.next = head;
+        ListNode startsPrev = headsPrev;
+
+        int i = 0;
+        while (i < n) {
+            end = end.next;
+            i++;
+        }
+        while (end != null) {
+            startsPrev = start;
+            start = start.next;
+            end = end.next;
+        }
+        startsPrev.next = start.next;
+        return headsPrev.next;
+    }
+
+    public ListNode removeNthFromEndNaive(ListNode head, int n) {
+        int length = getLength(head);
+        if (length-n-1 < 0) return head.next;
+        ListNode beforeNode = getNodeAtIndex(head, length, length-n-1);
+        removeNextNode(beforeNode);
+        return head;
+    }
+
+    private ListNode getNodeAtIndex(ListNode head, int length, int ind) {
+        for (int i = 0; i < length; i++) {
+            if (i == ind) {
                 break;
             }
-            prevNode = currentNode;
-            currentNode = currentNode.next;
-            index++;
-        }
-
-        return startNode.next;
-    }
-
-    private int countNumberOfNodes(ListNode head) {
-        int numberOfNodes = 0;
-        while (head != null) {
             head = head.next;
-            numberOfNodes++;
         }
-        return numberOfNodes;
+        return head;
     }
 
-    private int relativeZeroIndexStartNumberForLast(int last, int length) {
-        return length - last;
+    private void removeNextNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        head.next = head.next.next;
+    }
+
+    private int getLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
     }
 }
