62e834ce105dff6b90ebcccf5eaaebd3d652380f
govindev <vgovindev@gmail.com>
2024-10-07 21:43:00 +0530

    combination sum 1

--- src/com/sdesheet/blind/CombinationSum1.java
+++ src/com/sdesheet/blind/CombinationSum1.java
@@ -7,29 +7,34 @@ import java.util.List;
 public class CombinationSum1 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
+        List<Integer> combination = new ArrayList<>();
         List<List<Integer>> combinations = new ArrayList<>();
-        combinationSum(candidates, target, 0, new ArrayList<>(), combinations);
+
+        combinationSum(candidates, target, 0, combination, combinations);
+
         return combinations;
     }
 
-    public void combinationSum(int[] candidates, int target, int current,
+    public void combinationSum(int[] candidates, int target, int ind,
                                List<Integer> combination, List<List<Integer>> combinations) {
+
         if (target == 0) {
             combinations.add(new ArrayList<>(combination));
-        }
-        if (current >= candidates.length) {
             return;
         }
-        if (candidates[current] <= target) {
-            // Pick
-            combination.add(candidates[current]);
-            combinationSum(candidates, target - candidates[current], current, combination, combinations);
+        if (ind == candidates.length) {
+            return;
+        }
+
+        int candidate = candidates[ind];
+        if (candidate <= target) {
+            // I can pick
+            combination.add(candidate);
+            combinationSum(candidates, target - candidate, ind, combination, combinations);
             combination.removeLast();
         } else {
             return;
         }
-
-        // Not pick
-        combinationSum(candidates, target, current+1, combination, combinations);
+        combinationSum(candidates, target, ind + 1, combination, combinations);
     }
 }
