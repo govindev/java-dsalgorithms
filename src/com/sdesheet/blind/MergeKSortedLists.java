0109ddc2857eb13d5aa96a6659eea788afa8fcf3
govindev <vgovindev@gmail.com>
2024-10-08 22:43:01 +0530

    merge k sorted lists

--- src/com/sdesheet/blind/MergeKSortedLists.java
+++ src/com/sdesheet/blind/MergeKSortedLists.java
@@ -48,4 +48,49 @@ class MergeKSortedLists {
 
         return dummyNode.next;
     }
+
+
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        // 10(1)
+        // 5(1) + 5(1)
+        // 3(1) + 2(1) + 3(1) + 2(1)
+        // 2(1) + 1(0) + 1(0) + 1(0) + 2(1) + 1(0) + 1(0) + 1(0)
+        return merge(lists, 0, lists.length-1);
+    }
+
+
+    private ListNode merge(ListNode[] lists, int start, int end) {
+        if (start > lists.length-1 || end < 0) {
+            return null;
+        }
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = (start + end) / 2;
+        ListNode leftNode = merge(lists, start, mid);
+        ListNode rightNode = merge(lists, mid+1, end);
+        return merge(leftNode, rightNode);
+    }
+
+    private ListNode merge2(ListNode node1, ListNode node2) {
+        ListNode dummyNode = new ListNode();
+        ListNode current = dummyNode;
+        while (node1 != null && node2 != null) {
+            if (node1.val <= node2.val) {
+                current.next = node1;
+                node1 = node1.next;
+            } else {
+                current.next = node2;
+                node2 = node2.next;
+            }
+            current = current.next;
+        }
+        if (node1 != null) {
+            current.next = node1;
+        } else {
+            current.next = node2;
+        }
+        return dummyNode.next;
+    }
 }
