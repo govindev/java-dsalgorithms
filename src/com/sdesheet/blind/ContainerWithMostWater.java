3af47beeea6e3bcad4d8ae75c6eb5918c92541b9
govindev <vgovindev@gmail.com>
2024-10-09 21:55:47 +0530

    container with most water

--- src/com/sdesheet/blind/ContainerWithMostWater.java
+++ src/com/sdesheet/blind/ContainerWithMostWater.java
@@ -3,31 +3,22 @@ package com.sdesheet.blind;
 public class ContainerWithMostWater {
 	public static void main(String[] args) {
 		int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
-		System.out.println(maxArea(height));
+		ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+		System.out.println(containerWithMostWater.maxArea(height));
 	}
 
-	public static int maxArea1(int[] height) {
-		int max = 0;
-		for (int i = 0; i < height.length - 1; i++) {
-			for (int j = i + 1; j < height.length; j++) {
-				int water = (j - i) * Math.min(height[i], height[j]);
-				max = water > max ? water : max;
+	public int maxArea(int[] height) {
+		// Two pointer approach
+		int left = 0, right = height.length-1;
+		int maxWater = Integer.MIN_VALUE;
+		while (left < right) {
+			int currentWater = (right-left) * Math.min(height[left], height[right]);
+			maxWater = Math.max(maxWater, currentWater);
+			if (height[left] > height[right]) {
+				right--;
 			}
-		}
-		return max;
-	}
-
-	public static int maxArea(int[] height) {
-		int leftLine = 0, rightLine = height.length - 1;
-		int maxWater = 0;
-		while (leftLine < rightLine) {
-			int windowWater = Math.min(height[leftLine], height[rightLine]) * (rightLine - leftLine);
-			maxWater = Math.max(maxWater, windowWater);
-
-			if (height[leftLine] < height[rightLine]) {
-				leftLine++;
-			} else {
-				rightLine--;
+			else {
+				left++;
 			}
 		}
 		return maxWater;
