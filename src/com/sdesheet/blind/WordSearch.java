e04de44e28f9fe7a8449bc89c408caf9f4616745
govindev <vgovindev@gmail.com>
2024-10-10 19:24:01 +0530

    word search

---
+++ src/com/sdesheet/blind/WordSearch.java
@@ -0,0 +1,41 @@
+package com.sdesheet.blind;
+
+public class WordSearch {
+    public boolean exist(char[][] board, String word) {
+        boolean[][] used = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (exist(board, used, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean exist(char[][] board, boolean[][] used, int i, int j, String word, int ind) {
+        if (ind >= word.length() || i < 0 || i > board.length-1 || j < 0
+                || j > board[i].length-1 || used[i][j]) {
+            return false;
+        }
+
+        char key = word.charAt(ind);
+        if (key != board[i][j]) {
+            return false;
+        }
+        if (ind == word.length()-1) {
+            return true;
+        }
+
+        used[i][j] = true;
+        if (exist(board, used, i-1, j, word, ind+1) ||
+                exist(board, used, i, j+1, word, ind+1) ||
+                exist(board, used, i+1, j, word, ind+1) ||
+                exist(board, used, i, j-1, word, ind+1)) {
+            return true;
+        }
+        used[i][j] = false;
+
+        return false;
+    }
+}
