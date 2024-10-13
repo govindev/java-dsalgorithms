c755792a5403ab6a2596547eeadf46358728e168
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2024-10-13 09:55:41 +0530

    Grid Unique Paths

--- src/com/miscellaneous/UniquePaths.java
+++ src/com/miscellaneous/UniquePaths.java
@@ -1,46 +1,27 @@
 package com.miscellaneous;
 
+import java.util.Arrays;
+
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-//         return getUniquePathsBruteForce(0, 0, m-1, n-1);
-
-//         int[][] dp = new int[m][n];
-
-//         for (int[] dpArr : dp) {
-//             Arrays.fill(dpArr, -1);
-//         }
-
-//         return getUniquePathsDp(0, 0, m-1, n-1, dp);
-
-        int N = m + n -2;
-        int r = m-1;
-        double res = 1;
-        for (int i = 1; i <= r; i++) {
-            res = res * (N - r + i) / i;
+        int[][] dp = new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return (int) res;
+        return uniquePaths(0, 0, m, n, dp);
     }
 
-    public int getUniquePathsDp(int pm, int pn, int m, int n, int[][] dp) {
-        if (dp[pm][pn] != -1) return dp[pm][pn];
-
-        if (pm > m || pn > n) return 0;
-        if (pm == m || pn == n) return 1;
-
-        int rightPaths = getUniquePathsDp(pm, pn+1, m, n, dp);
-        int downPaths = getUniquePathsDp(pm+1, pn, m, n, dp);
-
-        dp[pm][pn] = rightPaths + downPaths;
-        return dp[pm][pn];
-    }
-
-    public int getUniquePathsBruteForce(int pm, int pn, int m, int n) {
-        if (pm > m || pn > n) return 0;
-        if (pm == m || pn == n) return 1;
-
-        int rightPaths = getUniquePathsBruteForce(pm, pn+1, m, n);
-        int downPaths = getUniquePathsBruteForce(pm+1, pn, m, n);
-
-        return rightPaths + downPaths;
+    private int uniquePaths(int i, int j, int rows, int cols, int[][] dp) {
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (i == rows-1 || j == cols-1) {
+            dp[i][j] = 1;
+        } else {
+            int rightPaths = uniquePaths(i, j+1, rows, cols, dp);
+            int bottomPaths = uniquePaths(i+1, j, rows, cols, dp);
+            dp[i][j] = rightPaths+bottomPaths;
+        }
+        return dp[i][j];
     }
 }
