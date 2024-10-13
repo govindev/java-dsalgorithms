98bf59bb5a01549266dd988a3fc7985673aad4d3
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2024-10-13 11:49:26 +0530

    binary tree maximum path sum

--- src/com/sdesheet/blind/BinaryTreeMaximumPathSum.java
+++ src/com/sdesheet/blind/BinaryTreeMaximumPathSum.java
@@ -4,24 +4,23 @@ public class BinaryTreeMaximumPathSum {
     int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         max = Integer.MIN_VALUE;
-        maximumPathSum(root);
+        maxPath(root);
         return max;
     }
 
-    private int maximumPathSum(TreeNode root) {
+    private int maxPath(TreeNode root) {
         if (root == null) {
             return 0;
         }
         int current = root.val;
-        int leftSum = maximumPathSum(root.left);
-        int rightSum = maximumPathSum(root.right);
+        int leftMaxPathSum = maxPath(root.left);
+        int rightMaxPathSum = maxPath(root.right);
 
         max = Math.max(max, current);
-        max = Math.max(max, current+leftSum);
-        max = Math.max(max, current+rightSum);
-        max = Math.max(max, current+leftSum+rightSum);
+        max = Math.max(max, current + leftMaxPathSum);
+        max = Math.max(max, current + rightMaxPathSum);
+        max = Math.max(max, current + leftMaxPathSum + rightMaxPathSum);
 
-        return Math.max(current, Math.max(current+leftSum, current+rightSum));
+        return Math.max(current, Math.max(current + leftMaxPathSum, current + rightMaxPathSum));
     }
-
 }
