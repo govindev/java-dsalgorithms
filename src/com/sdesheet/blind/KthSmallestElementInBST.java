ca4e4aab364908e053ffdab6580ddbc95024e05a
govindev <vgovindev@gmail.com>
2024-10-18 00:49:58 +0530

    kth smallest element in bst

--- src/com/sdesheet/blind/KthSmallestElementInBST.java
+++ src/com/sdesheet/blind/KthSmallestElementInBST.java
@@ -1,30 +1,23 @@
 package com.sdesheet.blind;
 
 public class KthSmallestElementInBST {
-    int counter = 0;
+    int ind;
     public int kthSmallest(TreeNode root, int k) {
-        counter = 0;
-        TreeNode smallest = kthSmallestNode(root, k);
-        if (smallest != null) {
-            return smallest.val;
-        }
-        return -1;
+        ind = 0;
+        return dfs(root, k).val;
     }
-
-    private TreeNode kthSmallestNode(TreeNode root, int k) {
+    private TreeNode dfs(TreeNode root, int k) {
         if (root == null) {
-            return null;
-        }
-        if (root.left != null) {
-            TreeNode left = kthSmallestNode(root.left, k);
-            if (left != null) {
-                return left;
-            }
-        }
-        counter++;
-        if (k == counter) {
             return root;
         }
-        return kthSmallestNode(root.right, k);
+        TreeNode res = dfs(root.left, k);
+        if (res != null) {
+            return res;
+        }
+        ind++;
+        if (ind == k) {
+            return root;
+        }
+        return dfs(root.right, k);
     }
 }
