4695d0b1010558445398b182f0bde4945873925c
govindev <vgovindev@gmail.com>
2024-10-18 21:12:36 +0530

    invert binary tree

--- src/com/sdesheet/blind/InvertBinaryTree.java
+++ src/com/sdesheet/blind/InvertBinaryTree.java
@@ -3,13 +3,11 @@ package com.sdesheet.blind;
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
-            return root;
+            return null;
         }
-
-        TreeNode invertedLeft = invertTree(root.left);
+        TreeNode left = root.left;
         root.left = invertTree(root.right);
-        root.right = invertedLeft;
-
+        root.right = invertTree(left);
         return root;
     }
 }
