2425c2fddd85360dbdc2b083fac7aa01e000dd87
govindev <vgovindev@gmail.com>
2024-10-20 12:17:13 +0530

    number of islands

--- src/com/sdesheet/blind/NumberOfIslands.java
+++ src/com/sdesheet/blind/NumberOfIslands.java
@@ -2,11 +2,16 @@ package com.sdesheet.blind;
 
 public class NumberOfIslands {
     public static void main(String[] args) {
-        // [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]
-        int[][] grid = new int[][] {{}, {}};
+        char[][] grid = new char[][] {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
         NumberOfIslands numberOfIslands = new NumberOfIslands();
         System.out.println("Number of islands : "+ numberOfIslands.numIslands(grid));
     }
+
     public int numIslands(char[][] grid) {
         int islands = 0;
         boolean[][] used = new boolean[grid.length][grid[0].length];
@@ -23,11 +28,12 @@ public class NumberOfIslands {
     private boolean dfs(char[][] grid, boolean[][] used, int i, int j) {
         if (i < 0 || i > grid.length-1
                 || j < 0 || j > grid[i].length-1
-                || grid[i][j] == 0
+                || grid[i][j] == '0'
                 || used[i][j]) {
             return false;
         }
         used[i][j] = true;
+
         dfs(grid, used, i-1, j);
         dfs(grid, used, i, j+1);
         dfs(grid, used, i+1, j);
@@ -35,4 +41,5 @@ public class NumberOfIslands {
 
         return true;
     }
+    
 }
