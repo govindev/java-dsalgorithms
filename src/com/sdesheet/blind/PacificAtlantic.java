9aaa0ed4a0afecb38f7b0ef9aed05421d17cee1f
govindev <vgovindev@gmail.com>
2024-10-24 21:05:31 +0530

    pacific atlantic water flow

--- src/com/sdesheet/blind/PacificAtlantic.java
+++ src/com/sdesheet/blind/PacificAtlantic.java
@@ -1,30 +1,38 @@
 package com.sdesheet.blind;
 
-public class PacificAtlantic {
-    List<List<Integer>> result;
-    boolean[][] pacificFlows;
-    boolean[][] atlanticFlows;
-    int[][] heights;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class PacificAtlantic {
+    int[][] heights;
+    Set<Integer> pacific;
+    Set<Integer> atlantic;
+    int rowLen;
+    int colLen;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         this.heights = heights;
-        pacificFlows = new boolean[heights.length][heights[0].length];
-        atlanticFlows = new boolean[heights.length][heights[0].length];
-        result = new ArrayList<>();
+        pacific = new HashSet<>();
+        atlantic = new HashSet<>();
+        rowLen = heights.length;
+        colLen = heights[0].length;
 
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[i].length; j++) {
-                pacificFlow(i, j);
-            }
+        for (int c = 0; c < colLen; c++) {
+            dfs(0, c, heights[0][c], pacific);
+            dfs(rowLen-1, c, heights[rowLen-1][c], atlantic);
         }
-        for (int i = heights.length-1; i >= 0; i--) {
-            for (int j = heights[i].length-1; j >= 0; j--) {
-                atlanticFlow(i, j);
-            }
+
+        for (int r = 0; r < rowLen; r++) {
+            dfs(r, 0, heights[r][0], pacific);
+            dfs(r, colLen-1, heights[r][colLen-1], atlantic);
         }
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[i].length; j++) {
-                if (pacificFlows[i][j] && atlanticFlows[i][j]) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                int key = key(i, j);
+                if (pacific.contains(key) && atlantic.contains(key)) {
                     result.add(List.of(i,j));
                 }
             }
@@ -32,52 +40,23 @@ public class PacificAtlantic {
         return result;
     }
 
-    private boolean pacificFlow(int i, int j) {
-        if (i < 0 || i > heights.length-1
-                || j < 0 || j > heights.length-1) {
-            return false;
+
+    private void dfs(int i, int j, int prevHeight, Set<Integer> visited) {
+        if (i < 0 || i > rowLen-1
+                || j < 0 || j > colLen-1
+                || visited.contains(key(i, j))
+                || heights[i][j] < prevHeight) {
+            return;
         }
-        if (i == 0 || j == 0 || pacificFlows[i][j]) {
-            pacificFlows[i][j] = true;
-            return true;
-        }
-        int height = heights[i][j];
-        if ((canExplore(i-1, j, height) && pacificFlow(i-1, j))
-                || (canExplore(i, j+1, height) && pacificFlow(i, j+1))
-                || (canExplore(i+1, j, height) && pacificFlow(i+1, j))
-                || (canExplore(i, j-1, height) && pacificFlow(i, j-1))) {
-            pacificFlows[i][j] = true;
-            return true;
-        }
-        return false;
+        visited.add(key(i,j));
+
+        dfs(i-1, j, heights[i][j], visited);
+        dfs(i, j+1, heights[i][j], visited);
+        dfs(i+1, j, heights[i][j], visited);
+        dfs(i, j-1, heights[i][j], visited);
     }
 
-    private boolean canExplore(int i, int j, int fromHeight) {
-        if (i < 0 || i > heights.length-1
-                || j < 0 || j > heights.length-1) {
-            return false;
-        }
-        return (fromHeight >= heights[i][j]);
+    private int key(int i, int j) {
+        return (i * colLen) + j;
     }
-
-    private boolean atlanticFlow(int i, int j) {
-        if (i < 0 || i > heights.length-1
-                || j < 0 || j > heights.length-1) {
-            return false;
-        }
-        if (i == heights.length-1 || j == heights.length-1 || atlanticFlows[i][j]) {
-            atlanticFlows[i][j] = true;
-            return true;
-        }
-        int height = heights[i][j];
-        if ((canExplore(i-1, j, height) && atlanticFlow(i-1, j))
-                || (canExplore(i, j+1, height) && atlanticFlow(i, j+1))
-                || (canExplore(i+1, j, height) && atlanticFlow(i+1, j))
-                || (canExplore(i, j-1, height) && atlanticFlow(i, j-1))) {
-            atlanticFlows[i][j] = true;
-            return true;
-        }
-        return false;
-    }
-
 }
